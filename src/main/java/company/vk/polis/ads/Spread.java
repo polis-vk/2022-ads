ca93897a14b8dcad0d306c0a061d23a8e9c8e158
StombieIT <stombie@yandex.ru>
2022-09-27 21:18:03 +0300

    initial commit

--- src/main/java/company/vk/polis/ads/Spread.java
+++ src/main/java/company/vk/polis/ads/Spread.java
@@ -12,14 +12,48 @@ import java.util.StringTokenizer;
  *
  * @author Dmitry Schitinin
  */
-public final class SolveTemplate {
-    private SolveTemplate() {
+public final class Spread {
+    private Spread() {
         // Should not be instantiated
     }
 
     private static void solve(final FastScanner in, final PrintWriter out) {
-        String inp = in.next();
-        out.println(inp);
+        final int N = in.nextInt();
+        int[] array = new int[N];
+        int min;
+        int max;
+        max = min = array[0];
+        for (int i = 1; i < N; i++) {
+            array[i] = in.nextInt();
+            if (array[i] < min) {
+                min = array[i];
+            }
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        int[] indexes = new int[max - min + 1];
+        for (int number : array) {
+            indexes[number - min]++;
+        }
+        // ?? int
+        int calculatedIndex = 0;
+        for (int i = 0; i < indexes.length; i++) {
+            int temp = indexes[i];
+            indexes[i] = calculatedIndex;
+            calculatedIndex += temp;
+        }
+
+        int[] sortedArray = new int[N];
+
+        for (int number : array) {
+            sortedArray[indexes[number - min]] = number;
+            indexes[number]++;
+        }
+
+        for (int number : sortedArray) {
+            out.println(number);
+        }
     }
 
     private static final class FastScanner {
