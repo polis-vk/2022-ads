fd7914eb395ae7a32dcf33977c339cbc4e44edd7
Vladislav Pochernin <vspochernin@gmail.com>
2022-09-28 16:54:33 +0300

    part2-task4: done

--- src/main/java/Part2Task4.java
+++ src/main/java/Part2Task4.java
@@ -4,6 +4,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.StringTokenizer;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * Problem solution template.
@@ -15,8 +16,71 @@ public final class Part2Task4 {
         // Should not be instantiated
     }
 
+    private static void quickSort(int[] a, int from, int to) {
+        if (from + 1 >= to) {
+            return;
+        }
+        int i = partition(a, from, to);
+        quickSort(a, from, i);
+        quickSort(a, i + 1, to);
+    }
+
+    private static int partition(int[] a, int from, int to) {
+        int i = ThreadLocalRandom.current().nextInt(to - from) + from;
+        swap(a, from, i);
+        int pivotal = a[from];
+        i = from;
+        for (int j = from + 1; j < to; j++) {
+            if (a[j] <= pivotal) {
+                swap(a, ++i, j);
+            }
+        }
+        swap(a, i, from);
+        return i;
+    }
+
+    private static void swap(int[] a, int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
     private static void solve(final FastScanner in, final PrintWriter out) {
-        // Write me
+        int n = in.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = in.nextInt();
+        }
+        n = in.nextInt();
+        int[] b = new int[n];
+        for (int i = 0; i < n; i++) {
+            b[i] = in.nextInt();
+        }
+
+        quickSort(a, 0, a.length);
+        quickSort(b, 0, b.length);
+
+        int i = 0;
+        int j = 0;
+        boolean isSimilar = true;
+        while (i < a.length && j < b.length) {
+            while (i + 1 < a.length && a[i + 1] == a[i]) {
+                i++;
+            }
+            while (j + 1 < b.length && b[j + 1] == b[j]) {
+                j++;
+            }
+            if (a[i] != b[j]) {
+                isSimilar = false;
+                break;
+            }
+            i++;
+            j++;
+        }
+        if (i < a.length || j < b.length) {
+            isSimilar = false;
+        }
+        out.println(isSimilar ? "YES" : "NO");
     }
 
     private static final class FastScanner {
