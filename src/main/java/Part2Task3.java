b682c110e1db16a3e22015fce271876db658f97d
Vladislav Pochernin <vspochernin@gmail.com>
2022-09-28 16:18:19 +0300

    part2-task3: done

--- src/main/java/Part2Task3.java
+++ src/main/java/Part2Task3.java
@@ -15,8 +15,70 @@ public final class Part2Task3 {
         // Should not be instantiated
     }
 
+    private static class Student implements Comparable<Student> {
+
+        int id;
+        int mark;
+
+        public Student(int id, int mark) {
+            this.id = id;
+            this.mark = mark;
+        }
+
+        @Override
+        public int compareTo(Student student) {
+            return mark != student.mark ? mark - student.mark : student.id - id;
+        }
+    }
+
+    private static void mergeSort(Student[] students, int from, int to, Student[] bufferedArray) {
+        if (from + 1 >= to) {
+            return;
+        }
+        int mid = from + ((to - from) >> 1);
+        mergeSort(students, from, mid, bufferedArray);
+        mergeSort(students, mid, to, bufferedArray);
+        merge(students, from, mid, to, bufferedArray);
+    }
+
+    private static void merge(Student[] students, int from, int mid, int to, Student[] bufferedArray) {
+        int i = from;
+        int j = mid;
+
+        while (i < mid && j < to) {
+            if (students[i].compareTo(students[j]) > 0) {
+                bufferedArray[i + j - from - mid] = students[i];
+                i++;
+            } else {
+                bufferedArray[i + j - from - mid] = students[j];
+                j++;
+            }
+        }
+        while (i < mid) {
+            bufferedArray[i + j - from - mid] = students[i];
+            i++;
+        }
+        while (j < to) {
+            bufferedArray[i + j - from - mid] = students[j];
+            j++;
+        }
+        for (i = from; i < to; i++) {
+            students[i] = bufferedArray[i - from];
+        }
+    }
+
     private static void solve(final FastScanner in, final PrintWriter out) {
-        // Write me
+        int n = in.nextInt();
+        Student[] students = new Student[n];
+        for (int i = 0; i < n; i++) {
+            int id = in.nextInt();
+            int mark = in.nextInt();
+            students[i] = new Student(id, mark);
+        }
+        mergeSort(students, 0, n, new Student[n]);
+        for (Student student : students) {
+            out.println(student.id + " " + student.mark);
+        }
     }
 
     private static final class FastScanner {
