f497196a73f5f6d272963e5d9c77b49153fc5c94
Vladislav Pochernin <vspochernin@gmail.com>
2022-09-28 12:14:15 +0300

    part2-task1: done

--- src/main/java/Part2Task1.java
+++ src/main/java/Part2Task1.java
@@ -15,8 +15,31 @@ public final class Part2Task1 {
         // Should not be instantiated
     }
 
+    private static final int COUNT_ARRAY_SIZE = 215;
+
+    private static int getCountArrayIndex(int number, int centralNumber) {
+        return number - centralNumber + COUNT_ARRAY_SIZE / 2;
+    }
+
+    private static int getNumber(int index, int centralNumber) {
+        return index + centralNumber - COUNT_ARRAY_SIZE / 2;
+    }
+
     private static void solve(final FastScanner in, final PrintWriter out) {
-        // Write me
+        int[] countArray = new int[COUNT_ARRAY_SIZE];
+        int N = in.nextInt();
+        int number = in.nextInt();
+        int centralNumber = number;
+        countArray[getCountArrayIndex(number, centralNumber)]++;
+        for (int i = 0; i < N - 1; i++) {
+            number = in.nextInt();
+            countArray[getCountArrayIndex(number, centralNumber)]++;
+        }
+        for (int i = 0; i < COUNT_ARRAY_SIZE; i++) {
+            for (int j = 0; j < countArray[i]; j++) {
+                out.print(getNumber(i, centralNumber) + " ");
+            }
+        }
     }
 
     private static final class FastScanner {
