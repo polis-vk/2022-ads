dc3bbe968664909609ceca71a221f22b84082d79
Egorka <trololo221@bk.ru>
2022-10-05 22:15:50 +0300

    Add solve for Heaping, HeapSort

---
+++ src/main/java/company/vk/polis/ads/part3/tedbear/HeapSort.java
@@ -0,0 +1,93 @@
+package company.vk.polis.ads.part3.tedbear;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class HeapSort {
+    private static void solve(final FastScanner in, final PrintWriter out) {
+        // Write me
+        int n = in.nextInt();
+        int[] array = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            array[i] = in.nextInt();
+        }
+
+        makeHeap(array);
+        heapSort(array);
+
+        for (int i = 1; i <= n; i++) {
+            out.write(array[i] + " ");
+        }
+
+    }
+
+    static void makeHeap(int[] a) {
+        int n = a.length - 1;
+        for (int k = n / 2; k >= 1; k--) {
+            sink(a, k, n);
+        }
+    }
+
+    static void heapSort(int[] a) {
+        int n = a.length - 1;
+        for (int k = n / 2; k >= 1; k--) {
+            sink(a, k, n);
+        }
+        while (n > 1) {
+            swap(a, 1, n--);
+            sink(a, 1, n);
+        }
+    }
+
+    public static void sink(int[] a, int k, int n) {
+        while (2 * k <= n) {
+            int j = 2 * k;
+            if (j < n && a[j] < a[j + 1]) {
+                j++;
+            }
+            if (a[k] >= a[j]) {
+                break;
+            }
+            swap(a, k, j);
+            k = j;
+        }
+    }
+
+    public static void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    private static final class FastScanner {
+        private final BufferedReader reader;
+        private StringTokenizer tokenizer;
+
+        FastScanner(final InputStream in) {
+            reader = new BufferedReader(new InputStreamReader(in));
+        }
+
+        String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+
+    public static void main(final String[] arg) {
+        final FastScanner in = new FastScanner(System.in);
+        try (PrintWriter out = new PrintWriter(System.out)) {
+            solve(in, out);
+        }
+    }
+}
---
+++ src/main/java/company/vk/polis/ads/part3/tedbear/Heaping.java
@@ -0,0 +1,118 @@
+package company.vk.polis.ads.part3.tedbear;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Heaping {
+    private static void solve(final FastScanner in, final PrintWriter out) {
+        // Write me
+        int quantityOfCommands = in.nextInt();
+        Heap heap = new Heap(quantityOfCommands);
+        int nextCommand;
+
+        for (int i = 0; i < quantityOfCommands; i++) {
+            nextCommand = in.nextInt();
+
+            switch (nextCommand) {
+                case 0 -> {
+                    int elementToAdd = in.nextInt();
+                    heap.insert(elementToAdd);
+                }
+                case 1 -> out.write(heap.delMax() + "\n");
+            }
+        }
+
+    }
+
+    public static class Heap {
+        private int size;
+        private int lastIndex = 0;
+        private final int[] storage;
+
+        public Heap(int size) {
+            this.size = ++size;
+            storage = new int[size];
+        }
+
+        public void insert (int element) {
+            storage[++lastIndex] = element;
+            swim(lastIndex);
+        }
+
+        private void swim(int n) {
+            int indexOfNewElement = n;
+
+            while (indexOfNewElement > 1 && storage[indexOfNewElement] > storage[indexOfNewElement / 2]) {
+                swap(storage, indexOfNewElement, indexOfNewElement / 2);
+                indexOfNewElement = indexOfNewElement / 2;
+            }
+        }
+
+        public int delMax() {
+            int maxElement = storage[1];
+            storage[1] = storage[lastIndex];
+
+            if (lastIndex == 1) {
+                storage[lastIndex] = 0;
+                lastIndex--;
+            } else {
+                storage[lastIndex] = 0;
+                lastIndex--;
+                sink(1);
+            }
+
+            return maxElement;
+        }
+
+        private void sink(int k) {
+            while (2 * k <= size) {
+                int j = 2 * k;
+                if (j < size && storage[j] < storage[j + 1]) {
+                    j++;
+                }
+                if (storage[k] >= storage[j]) {
+                    break;
+                }
+                swap(storage, k, j);
+                k = j;
+            }
+        }
+    }
+
+    public static void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    private static final class FastScanner {
+        private final BufferedReader reader;
+        private StringTokenizer tokenizer;
+
+        FastScanner(final InputStream in) {
+            reader = new BufferedReader(new InputStreamReader(in));
+        }
+
+        String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+
+    public static void main(final String[] arg) {
+        final FastScanner in = new FastScanner(System.in);
+        try (PrintWriter out = new PrintWriter(System.out)) {
+            solve(in, out);
+        }
+    }
+}
