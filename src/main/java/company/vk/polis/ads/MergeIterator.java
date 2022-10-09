9c581e1e8cf770b8c9fea03963ef9f6ea7d2b86b
sold666 <sold666@github.com>
2022-10-09 14:34:34 +0300

    add solutions that have passed the tests

--- src/main/java/company/vk/polis/ads/MergeIterator.java
+++ src/main/java/company/vk/polis/ads/MergeIterator.java
@@ -1,7 +1,6 @@
 package company.vk.polis.ads;
 
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 /**
  * Iterator that merges k input iterators ordered ascending.
@@ -11,7 +10,76 @@ import java.util.List;
  * @param <T> type of elements
  */
 public final class MergeIterator<T extends Comparable<T>> implements Iterator<T> {
-    private final List<Iterator<T>> iterators;
+    private final Heap minHeap;
+
+    private class Temp {
+        private final T value;
+        private final Iterator<T> iterator;
+
+        public Temp(T val, Iterator<T> iter) {
+            this.value = val;
+            this.iterator = iter;
+        }
+
+        public T value() {
+            return value;
+        }
+
+        public Iterator<T> iterator() {
+            return iterator;
+        }
+    }
+
+    public class Heap {
+
+        private final List<Temp> heapList;
+        private int currentSize;
+
+        public Heap(int size) {
+            this.currentSize = 0;
+            this.heapList = new ArrayList<>();
+            for (int i = 0; i < size + 1; i++) {
+                heapList.add(new Temp(null, null));
+            }
+        }
+
+        public void insert(Temp value) {
+            heapList.set(++currentSize, value);
+            swim(currentSize);
+        }
+
+        public Temp exctract() {
+            Temp root = heapList.get(1);
+            Collections.swap(heapList, 1, currentSize--);
+            sink(1);
+            return root;
+        }
+
+        private void swim(int k) {
+            while (k > 1 && heapList.get(k).value().compareTo(heapList.get(k / 2).value()) < 0) {
+                Collections.swap(heapList, k, k / 2);
+                k /= 2;
+            }
+        }
+
+        private void sink(int k) {
+            while (2 * k <= currentSize) {
+                int j = 2 * k;
+                if (j < currentSize && heapList.get(j + 1).value().compareTo(heapList.get(j).value()) < 0) {
+                    j++;
+                }
+                if (heapList.get(k).value().compareTo(heapList.get(j).value()) <= 0) {
+                    break;
+                }
+                Collections.swap(heapList, k, j);
+                k = j;
+            }
+        }
+
+        public int getSize() {
+            return currentSize;
+        }
+    }
 
     /**
      * Constructor
@@ -19,12 +87,17 @@ public final class MergeIterator<T extends Comparable<T>> implements Iterator<T>
      * @param iterators k input iterators of ascending ordered elements
      */
     public MergeIterator(List<Iterator<T>> iterators) {
-        this.iterators = iterators;
+        minHeap = new Heap(iterators.size());
+        for (Iterator<T> element : iterators) {
+            if (element.hasNext()) {
+                minHeap.insert(new Temp(element.next(), element));
+            }
+        }
     }
 
     @Override
     public boolean hasNext() {
-        throw new UnsupportedOperationException("Implement me");
+        return minHeap.getSize() > 0;
     }
 
     /**
@@ -34,6 +107,14 @@ public final class MergeIterator<T extends Comparable<T>> implements Iterator<T>
      */
     @Override
     public T next() {
-        throw new UnsupportedOperationException("Implement me");
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        var minElement = minHeap.exctract();
+        var iterator = minElement.iterator();
+        if (iterator.hasNext()) {
+            minHeap.insert(new Temp(iterator.next(), iterator));
+        }
+        return minElement.value();
     }
 }
--- src/main/java/company/vk/polis/ads/TopK.java
+++ src/main/java/company/vk/polis/ads/TopK.java
@@ -1,19 +1,89 @@
 package company.vk.polis.ads;
 
-import java.util.List;
+import java.util.*;
 
 /**
  * Class that allows us to get k max elements ordered descending in source list.
  */
+class Heap<T extends Comparable<T>> {
+
+    private final List<T> heapList;
+    private int currentSize;
+
+    public Heap(int size) {
+        this.currentSize = 0;
+        this.heapList = new ArrayList<>();
+        for (int i = 0; i < size + 1; i++) {
+            heapList.add(null);
+        }
+    }
+
+    public void insert(T value) {
+        heapList.set(++currentSize, value);
+        swim(currentSize);
+    }
+
+    public T exctract() {
+        if (currentSize == 0) {
+            throw new RuntimeException();
+        }
+        if (currentSize == 1) {
+            currentSize--;
+        }
+        T root = heapList.get(1);
+        Collections.swap(heapList, 1, currentSize--);
+        sink(1);
+        return root;
+    }
+
+    private void swim(int k) {
+        while (k > 1 && heapList.get(k).compareTo(heapList.get(k / 2)) < 0) {
+            Collections.swap(heapList, k, k / 2);
+            k /= 2;
+        }
+    }
+
+    private void sink(int k) {
+        while (2 * k <= currentSize) {
+            int j = 2 * k;
+            if (j < currentSize && heapList.get(j + 1).compareTo(heapList.get(j)) < 0) {
+                j++;
+            }
+            if (heapList.get(k).compareTo(heapList.get(j)) <= 0) {
+                break;
+            }
+            Collections.swap(heapList, k, j);
+            k = j;
+        }
+    }
+
+    public T getFirstElement() {
+        return heapList.get(1);
+    }
+
+    public int getSize() {
+        return currentSize;
+    }
+}
+
 public final class TopK {
-    /**
-     * Top-K with O(n logk) complexity.
-     * @param list input list
-     * @param k amount of max values to carry out
-     * @return list with k max elements ordered descending
-     * @param <T> type of elements
-     */
     public <T extends Comparable<T>> List<T> topK(List<T> list, int k) {
-        throw new UnsupportedOperationException("Implement me");
+        Heap<T> minHeap = new Heap<>(k);
+        ListIterator<T> listIterator = list.listIterator();
+        while (listIterator.hasNext() && minHeap.getSize() < k) {
+            minHeap.insert(listIterator.next());
+        }
+        while (listIterator.hasNext()) {
+            if (listIterator.next().compareTo(minHeap.getFirstElement()) > 0) {
+                minHeap.exctract();
+                minHeap.insert(listIterator.next());
+            }
+        }
+        List<T> topKList = new ArrayList<>();
+        while (minHeap.getSize() > 0) {
+            topKList.add(minHeap.exctract());
+        }
+        Collections.reverse(topKList);
+        return topKList;
     }
 }
