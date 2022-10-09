2843e58679f4d43433b77781e35632d3dee1afd0
Ilgiz2003 <97854970+Ilgiz2003@users.noreply.github.com>
2022-10-09 22:21:46 +0300

    Add package tasks

--- src/main/java/company/vk/polis/ads/MinHeap.java
+++ src/main/java/company/vk/polis/ads/MinHeap.java
@@ -1,2 +1,104 @@
-package company.vk.polis.ads;public class MinHeap {
+package company.vk.polis.ads;
+
+
+import java.util.*;
+
+public class MinHeap<T extends Comparable<T>> {
+    private Object[] heap;
+    private int heapSize;
+    private static final int two = 2;
+    public MinHeap(int size) {
+        this.heapSize = 0;
+        heap = (new Object[size]);
+    }
+
+    public Object[] getHeap() {
+        return heap;
+    }
+
+    private T getModified(int i) {
+        return (T)heap[i];
+    }
+
+    private int parent(int i) {
+        if (i == 0) {
+            return 0;
+        }
+        return (i - 1) / two;
+    }
+
+    private int leftChild(int i) {
+        return 2 * i + 1;
+    }
+
+    private int rightChild(int i) {
+        return 2 * i + 2;
+    }
+
+    private void swap(Object[] heap, int i, int j) {
+        Object temp = heap[i];
+        heap[i] = heap[j];
+        heap[j] = temp;
+    }
+
+    private void heapifyUp(int i) {
+        if (i > 0 && getModified(parent(i)).compareTo(getModified(i)) < 0) {
+            swap(heap, i, parent(i));
+            heapifyUp(parent(i));
+        }
+    }
+
+    private void heapifyDown(int i) {
+        int left = leftChild(i);
+        int right = rightChild(i);
+        int smallest = i;
+        if (left < heapSize && getModified(left).compareTo(getModified(i)) < 0) {
+            smallest = left;
+        }
+        if (right < heapSize && getModified(right).compareTo(getModified(smallest)) < 0) {
+            smallest = right;
+        }
+        if (smallest != i) {
+            swap(heap, i, smallest);
+            heapifyDown(smallest);
+        }
+    }
+
+    public void insert(T element) {
+        if (heapSize == heap.length - 1) {
+            heap = Arrays.copyOf(heap, heap.length * two);
+        }
+        heapSize++;
+        heap[heapSize - 1] = element;
+        heapifyUp(heapSize - 1);
+    }
+
+    public T extract() {
+        if (heapSize == 0) {
+            throw new NoSuchElementException();
+        }
+        T min = getFirstElement();
+        heap[0] = getModified(heapSize - 1);
+        heapSize--;
+        heapifyDown(0);
+        return min;
+    }
+
+    public T getFirstElement() {
+        return getModified(0);
+    }
+
+    public int getHeapSize() {
+        return heapSize;
+    }
+
+    public List<T> toList(Comparator<T> comparator){
+        List<T> list = new ArrayList<>();
+        for (int i = 0; i< heapSize;i++){
+            list.add(getModified(i));
+        }
+        list.sort(comparator);
+        return list;
+    }
+
 }
--- src/main/java/company/vk/polis/ads/TopK.java
+++ src/main/java/company/vk/polis/ads/TopK.java
@@ -1,5 +1,6 @@
 package company.vk.polis.ads;
 
+import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -8,12 +9,32 @@ import java.util.List;
 public final class TopK {
     /**
      * Top-K with O(n logk) complexity.
+     *
      * @param list input list
      * @param k amount of max values to carry out
      * @return list with k max elements ordered descending
      * @param <T> type of elements
      */
+    private static final int maxSize = 1000000;
+
     public <T extends Comparable<T>> List<T> topK(List<T> list, int k) {
-        throw new UnsupportedOperationException("Implement me");
+        MinHeap<T> minHeap = new MinHeap<>(maxSize);
+        for (int i = 0; i < k; i++) {
+            minHeap.insert(list.get(i));
+        }
+
+        for (int i = k; i < list.size(); i++) {
+            T element = list.get(i);
+            if (element.compareTo(minHeap.getFirstElement()) > 0) {
+                minHeap.extract();
+                minHeap.insert(element);
+            }
+        }
+        return minHeap.toList(new Comparator<T>() {
+            @Override
+            public int compare(T o1, T o2) {
+                return -o1.compareTo(o2);
+            }
+        });
     }
 }
