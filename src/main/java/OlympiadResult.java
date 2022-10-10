26b8bf9f4d640d401b43fa73c31c478a0bd65ddf
IvAlexsay <ivalexsay@gmail.com>
2022-10-10 13:39:53 +0300

    Исправлена высокая аллокация памяти в MergeSort

--- src/main/java/OlympiadResult.java
+++ src/main/java/OlympiadResult.java
@@ -20,46 +20,46 @@ public class OlympiadResult {
         }
     }
 
-    private static void MergeSort(Result[] arr, int left, int right) {
+    private void MergeSort(Result[] arr, int left, int right, Result[] temp) {
         if (left + 1 >= right) {
             return;
         }
         int mid = (left + right) / 2;
-        MergeSort(arr, left, mid);
-        MergeSort(arr, mid, right);
-        merge(arr, left, mid, right);
+        MergeSort(arr, left, mid, temp);
+        MergeSort(arr, mid, right, temp);
+        merge(arr, left, mid, right, temp);
     }
 
-    private static void merge(Result[] arr, int left, int mid, int right) {
+    private void merge(Result[] arr, int left, int mid, int right, Result[] temp) {
         int firstIter = 0;
         int secondIter = 0;
-        Result[] mergeResult = new Result[right - left];
 
         while (left + firstIter < mid && mid + secondIter < right) {
             if (arr[left + firstIter].compareTo(arr[mid + secondIter]) >= 0) {
-                mergeResult[firstIter + secondIter] = arr[left + firstIter];
+                temp[firstIter + secondIter] = arr[left + firstIter];
                 firstIter++;
                 continue;
             }
-            mergeResult[firstIter + secondIter] = arr[mid + secondIter];
+            temp[firstIter + secondIter] = arr[mid + secondIter];
             secondIter++;
         }
 
         while (left + firstIter < mid) {
-            mergeResult[firstIter + secondIter] = arr[left + firstIter];
+            temp[firstIter + secondIter] = arr[left + firstIter];
             firstIter++;
         }
 
         while (mid + secondIter < right) {
-            mergeResult[firstIter + secondIter] = arr[mid + secondIter];
+            temp[firstIter + secondIter] = arr[mid + secondIter];
             secondIter++;
         }
 
-        System.arraycopy(mergeResult, 0, arr, left, firstIter + secondIter);
+        System.arraycopy(temp, 0, arr, left, firstIter + secondIter);
     }
 
-    private static void solve(Result[] results) {
-        MergeSort(results, 0, results.length);
+    private void solve(Result[] results) {
+        Result[] tempArr = new Result[results.length];
+        MergeSort(results, 0, results.length, tempArr);
         for (Result result : results) {
             System.out.println(result.id + " " + result.score);
         }
