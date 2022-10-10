720beb60a17838aaaf1c8942f10eb17f0d6a51a9
Denis Strizhkin <strdenis02@gmail.com>
2022-10-11 00:36:48 +0300

    Implemented MergeIterator functions.

--- src/main/java/company/vk/polis/ads/MergeIterator.java
+++ src/main/java/company/vk/polis/ads/MergeIterator.java
@@ -2,6 +2,7 @@ package company.vk.polis.ads;
 
 import java.util.Iterator;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 /**
  * Iterator that merges k input iterators ordered ascending.
@@ -12,6 +13,9 @@ import java.util.List;
  */
 public final class MergeIterator<T extends Comparable<T>> implements Iterator<T> {
     private final List<Iterator<T>> iterators;
+    private final MinHeap<Pair<T>> heap;
+    private boolean areIteratorsEmpty;
+
 
     /**
      * Constructor
@@ -19,12 +23,28 @@ public final class MergeIterator<T extends Comparable<T>> implements Iterator<T>
      * @param iterators k input iterators of ascending ordered elements
      */
     public MergeIterator(List<Iterator<T>> iterators) {
+        this.heap = new MinHeap<>();
         this.iterators = iterators;
+
+        for (int i = 0; i < iterators.size(); i++) {
+            if (iterators.get(i).hasNext()) {
+                heap.insert(new Pair<>(iterators.get(i).next(), i));
+            }
+        }
     }
 
     @Override
     public boolean hasNext() {
-        throw new UnsupportedOperationException("Implement me");
+        areIteratorsEmpty = true;
+
+        for (Iterator<T> iterator : iterators) {
+            if (iterator.hasNext()) {
+                areIteratorsEmpty = false;
+                break;
+            }
+        }
+
+        return !areIteratorsEmpty || heap.size() != 0;
     }
 
     /**
@@ -34,6 +54,40 @@ public final class MergeIterator<T extends Comparable<T>> implements Iterator<T>
      */
     @Override
     public T next() {
-        throw new UnsupportedOperationException("Implement me");
+        if (!this.hasNext()) {
+            throw new NoSuchElementException();
+        }
+
+        Pair<T> nextPair = heap.delMin();
+        if (areIteratorsEmpty) {
+            return nextPair.value;
+        }
+
+        if (iterators.get(nextPair.iteratorIndex).hasNext()) {
+            heap.insert(new Pair<>(iterators.get(nextPair.iteratorIndex).next(), nextPair.iteratorIndex));
+        } else {
+            for (int i = 0; i < iterators.size(); i++) {
+                if (iterators.get(i).hasNext()) {
+                    heap.insert(new Pair<>(iterators.get(i).next(), i));
+                    break;
+                }
+            }
+        }
+
+        return nextPair.value;
+    }
+
+    private class Pair<TT extends Comparable<TT>> implements Comparable<Pair<TT>> {
+        private final T value;
+        private final int iteratorIndex;
+
+        public Pair(T value, int iteratorIndex) {
+            this.value = value;
+            this.iteratorIndex = iteratorIndex;
+        }
+        @Override
+        public int compareTo(Pair<TT> pair) {
+            return this.value.compareTo(pair.value);
+        }
     }
 }
