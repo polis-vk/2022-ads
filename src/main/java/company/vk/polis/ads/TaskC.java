a3164701286ff9f4814aeed0df2a74dc842858c9
osokindm <osokin34@gmail.com>
2022-10-10 21:21:13 +0300

    Osokin Dmitry 2nd hw + corrections

--- src/main/java/company/vk/polis/ads/TaskC.java
+++ src/main/java/company/vk/polis/ads/TaskC.java
@@ -1,42 +1,88 @@
 package company.vk.polis.ads;
 
 import java.io.*;
+import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class TaskC {
     private static void solve(final FastScanner in, final PrintWriter out) {
         int n = in.nextInt();
-        int[][] students = new int[n][2];
+        Student[] students = new Student[n];
+        temp = new Student[n];
         for (int i = 0; i < n; i++) {
-            students[i][0] = in.nextInt();
-            students[i][1] = in.nextInt();
+            students[i] = new Student(in.nextInt(), in.nextInt());
         }
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = i; j < n; j++) {
-                if (students[i][1] < students[j][1]) {
-                    int temp0, temp1;
-                    temp0 = students[i][0];
-                    temp1 = students[i][1];
-                    students[i][0] = students[j][0];
-                    students[i][1] = students[j][1];
-                    students[j][0] = temp0;
-                    students[j][1] = temp1;
-                } else if (students[i][1] == students[j][1]) {
-                    if (students[i][0] > students[j][0]) {
-                        int temp0;
-                        temp0 = students[i][0];
-                        students[i][0] = students[j][0];
-                        students[j][0] = temp0;
-                    }
-                }
-            }
-        }
+        mergeSort(students, 0, students.length);
+
         for (int i = 0; i < n; i++) {
-            System.out.println(students[i][0] + " " + students[i][1]);
+            System.out.println(students[i].getId() + " " + students[i].getPoints());
         }
 
     }
 
+    private static void mergeSort(Student[] students, int fromInclusive, int toExclusive) {
+        if (fromInclusive == toExclusive - 1) {
+            return;
+        }
+        int mid = fromInclusive + ((toExclusive - fromInclusive) >> 1);
+        mergeSort(students, fromInclusive, mid);
+        mergeSort(students, mid, toExclusive);
+        merge(students, fromInclusive, mid, toExclusive);
+    }
+
+    private static Student[] temp;
+
+    private static void merge(Student[] students, int fromInclusive, int mid, int toInclusive) {
+
+        int leftBorder = fromInclusive;
+        int rightBorder = mid;
+
+        while (leftBorder < mid && rightBorder < toInclusive) {
+            if (students[leftBorder].compareTo(students[rightBorder]) > 0) {
+                temp[leftBorder + rightBorder - fromInclusive - mid] = students[leftBorder];
+                leftBorder++;
+            } else {
+                temp[leftBorder + rightBorder - fromInclusive - mid] = students[rightBorder];
+                rightBorder++;
+            }
+        }
+        while (leftBorder < mid) {
+            temp[leftBorder + rightBorder - fromInclusive - mid] = students[leftBorder];
+            leftBorder++;
+        }
+        while (rightBorder < toInclusive) {
+            temp[leftBorder + rightBorder - fromInclusive - mid] = students[rightBorder];
+            rightBorder++;
+        }
+        for (leftBorder = fromInclusive; leftBorder < toInclusive; leftBorder++) {
+            students[leftBorder] = temp[leftBorder - fromInclusive];
+        }
+    }
+
+    private static class Student implements Comparable<Student> {
+
+        private final int id;
+        private final int points;
+
+        public Student(int id, int points) {
+            this.id = id;
+            this.points = points;
+        }
+
+        public int getPoints() {
+            return points;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        @Override
+        public int compareTo(Student o) {
+            return Comparator.comparing(Student::getPoints).thenComparing(Student::getId, Comparator.reverseOrder())
+                    .compare(this, o);
+        }
+    }
 
     private static final class FastScanner {
         private final BufferedReader reader;
--- src/main/java/company/vk/polis/ads/TaskD.java
+++ src/main/java/company/vk/polis/ads/TaskD.java
@@ -1,27 +1,66 @@
 package company.vk.polis.ads;
 
 import java.io.*;
-import java.util.HashSet;
+import java.util.Arrays;
 import java.util.StringTokenizer;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class TaskD {
     private static boolean solve(final FastScanner in) {
-        HashSet<Integer> set1 = new HashSet<>();
+
         int n1 = in.nextInt();
+        int[] array1 = new int[n1];
         for (int i = 0; i < n1; i++) {
-            set1.add(in.nextInt());
+            array1[i] = in.nextInt();
         }
 
-        HashSet<Integer> set2 = new HashSet<>();
         int n2 = in.nextInt();
+        int[] array2 = new int[n2];
         for (int i = 0; i < n2; i++) {
-            set2.add(in.nextInt());
+            array2[i] = in.nextInt();
         }
 
-        return set1.equals(set2);
+        array1 = Arrays.stream(array1).distinct().toArray();
+        array2 = Arrays.stream(array2).distinct().toArray();
 
+        quickSort(array1, 0, array1.length);
+        quickSort(array2, 0, array2.length);
+
+        return Arrays.equals(array2, array1);
     }
 
+    private static void quickSort(int[] array, int fromInclusive, int toExclusive) {
+        if (fromInclusive >= toExclusive - 1) {
+            return;
+        }
+        int i = randomPartition(array, fromInclusive, toExclusive);
+        quickSort(array, fromInclusive, i);
+        quickSort(array, i + 1, toExclusive);
+    }
+
+    private static int randomPartition(int[] array, int fromInclusive, int toExclusive) {
+        int i = ThreadLocalRandom.current().nextInt(toExclusive - fromInclusive) + fromInclusive;
+        swap(array, fromInclusive, i);
+        return partition(array, fromInclusive, toExclusive);
+    }
+
+    private static int partition(int[] array, int fromInclusive, int toExclusive) {
+        int pivotal = array[fromInclusive];
+        int i = fromInclusive;
+        for (int j = fromInclusive + 1; j < toExclusive; ++j) {
+            if (array[j] <= pivotal) {
+                swap(array, ++i, j);
+            }
+        }
+        swap(array, i, fromInclusive);
+        return i;
+    }
+
+    private static void swap(int[] array, int i1, int i2) {
+        int temp = array[i1];
+        array[i1] = array[i2];
+        array[i2] = temp;
+    }
 
     private static final class FastScanner {
         private final BufferedReader reader;
