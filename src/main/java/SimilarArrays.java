6428b3d358f04e5eac169def0e8383cd36c2c49a
IvAlexsay <ivalexsay@gmail.com>
2022-10-10 14:48:48 +0300

    SimilarArrays реализован через быструю сортировку

--- src/main/java/SimilarArrays.java
+++ src/main/java/SimilarArrays.java
@@ -1,25 +1,52 @@
-import java.util.HashSet;
-import java.util.Set;
-
 public class SimilarArrays {
-    public void checkArraysOnSimilar(int[] firstArr, int[] secondArr) {
-        long firstSum = 0;
-        Set<Integer> firstNumbers = new HashSet<>();
-        for (int number : firstArr) {
-            if (!firstNumbers.contains(number)) {
-                firstNumbers.add(number);
-                firstSum += number;
+    private void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+
+    private int partition(int[] arr, int l, int r) {
+        int pivotal = arr[l];
+        int i = l;
+        for (int j = l + 1; j < r; j++) {
+            if (arr[j] <= pivotal) {
+                swap(arr, ++i, j);
             }
         }
-        int secondSum = 0;
-        Set<Integer> secondNumbers = new HashSet<>();
-        for (int number : secondArr) {
-            if (!secondNumbers.contains(number)) {
-                secondNumbers.add(number);
-                secondSum += number;
+        swap(arr, i, l);
+        return i;
+    }
+
+    private void quickSort(int[] arr, int l, int r) {
+        if (l >= r - 1) {
+            return;
+        }
+        int i = partition(arr, l, r);
+        quickSort(arr, l, i);
+        quickSort(arr, i + 1, r);
+    }
+
+    public void checkArraysOnSimilar(int[] firstArr, int[] secondArr) {
+        quickSort(firstArr, 0, firstArr.length);
+        quickSort(secondArr, 0, secondArr.length);
+        int firstSum = firstArr[0];
+        int last = firstArr[0];
+        for (int i = 1; i < firstArr.length; i++) {
+            if (firstArr[i] == last) {
+                continue;
             }
+            firstSum += firstArr[i];
+            last = firstArr[i];
+        }
+        int secondSum = secondArr[0];
+        last = secondArr[0];
+        for (int i = 1; i < secondArr.length; i++) {
+            if (secondArr[i] == last) {
+                continue;
+            }
+            secondSum += secondArr[i];
+            last = secondArr[i];
         }
         System.out.println(firstSum == secondSum ? "YES" : "NO");
     }
-
 }
