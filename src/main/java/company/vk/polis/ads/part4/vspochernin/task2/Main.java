2bb66fbf42db2b6cc08847d7b1fe23a49f66b5f5
Vladislav Pochernin <vspochernin@gmail.com>
2022-10-12 16:35:09 +0300

    part4-task2: done

--- src/main/java/company/vk/polis/ads/part4/vspochernin/task2/Main.java
+++ src/main/java/company/vk/polis/ads/part4/vspochernin/task2/Main.java
@@ -10,6 +10,8 @@ import java.util.StringTokenizer;
 /**
  * Problem solution template.
  *
+ * https://www.eolymp.com/ru/submissions/11761098
+ *
  * @author Dmitry Schitinin
  */
 public final class Main {
@@ -18,7 +20,57 @@ public final class Main {
     }
 
     private static void solve(final FastScanner in, final PrintWriter out) {
-        // Write me
+        int m = in.nextInt();
+        int n = in.nextInt();
+
+        int[][] grains = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                grains[m - 1 - i][j] = in.nextInt();
+            }
+        }
+
+        int[][] result = new int[m][n];
+        result[0][0] = grains[0][0];
+
+        for (int i = 1; i < m; i++) { // Идем по левой стенке вверх.
+            result[i][0] = result[i - 1][0] + grains[i][0];
+        }
+        for (int j = 1; j < n; j++) { // Идем по нижней стенке вправо.
+            result[0][j] = result[0][j - 1] + grains[0][j];
+        }
+
+        for (int i = 1; i < m; i++) { // Заполняем все поле.
+            for(int j = 1; j < n; j++) {
+                result[i][j] = Math.max(result[i-1][j], result[i][j-1]) + grains[i][j];
+            }
+        }
+
+        // Восстанавливаем ответ.
+
+        StringBuilder stringBuilder = new StringBuilder();
+        int i = m - 1;
+        int j = n - 1;
+
+        while (i > 0 && j > 0) {
+            if (result[i][j-1] > result[i-1][j]) {
+                stringBuilder.append("R");
+                j--;
+            } else {
+                stringBuilder.append("F");
+                i--;
+            }
+        }
+        while (i > 0) {
+            stringBuilder.append("F");
+            i--;
+        }
+        while (j > 0) {
+            stringBuilder.append("R");
+            j--;
+        }
+
+        out.println(stringBuilder.reverse().toString());
     }
 
     private static final class FastScanner {
