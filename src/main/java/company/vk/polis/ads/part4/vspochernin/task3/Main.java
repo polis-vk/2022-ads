b162cd085c6e54b3ed7761a43130e91f3a009c2a
Vladislav Pochernin <vspochernin@gmail.com>
2022-10-12 16:03:53 +0300

    part4-task3: done

--- src/main/java/company/vk/polis/ads/part4/vspochernin/task3/Main.java
+++ src/main/java/company/vk/polis/ads/part4/vspochernin/task3/Main.java
@@ -10,6 +10,8 @@ import java.util.StringTokenizer;
 /**
  * Problem solution template.
  *
+ * https://www.eolymp.com/ru/submissions/11760755
+ *
  * @author Dmitry Schitinin
  */
 public final class Main {
@@ -18,7 +20,30 @@ public final class Main {
     }
 
     private static void solve(final FastScanner in, final PrintWriter out) {
-        // Write me
+        int n = in.nextInt();
+        int[] a = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            a[i] = in.nextInt();
+        }
+
+        int m = in.nextInt();
+        int[] b = new int[m + 1];
+        for (int i = 1; i <= m; i++) {
+            b[i] = in.nextInt();
+        }
+
+        int[][] result = new int[n + 1][m + 1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (a[i] == b[j]) {
+                    result[i][j] = 1 + result[i - 1][j - 1];
+                } else {
+                    result[i][j] = Math.max(result[i - 1][j], result[i][j - 1]);
+                }
+            }
+        }
+
+        out.println(result[n][m]);
     }
 
     private static final class FastScanner {
