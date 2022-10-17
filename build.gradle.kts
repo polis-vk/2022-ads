5a05cda5f4e14041bb2ec1580c1e8a9a2724a3b4
AlexAndrovSPBSTU <AleksandrovAlexey0929@yandex.ru>
2022-10-18 00:30:02 +0300

    Revert "Merge branch 'main' into part4"
    This reverts commit 6851f9882bba2369354c79e743b132d238a8c173, reversing
    changes made to 62dd73dcd724ac589c26891cfd1c1523e474c3bd.

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,12 +16,10 @@ java {
 }
 
 dependencies {
-    testImplementation("it.unimi.dsi:fastutil:8.5.9")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
 }
 
 tasks.getByName<Test>("test") {
     useJUnitPlatform()
-    this.maxHeapSize = "1g"
 }
