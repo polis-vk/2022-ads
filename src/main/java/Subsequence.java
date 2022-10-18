a90baee1199c3c8a47f319195274d7f02e9d251c
Coo3n <Khlamov_ilya@mail.ru>
2022-10-19 00:16:42 +0300

    add

---
+++ src/main/java/Subsequence.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Subsequence {
+
+    private static void solve(final FastScanner in, final PrintWriter out) {
+
+    }
+
+    private static final class FastScanner {
+        private final BufferedReader reader;
+        private StringTokenizer tokenizer;
+
+        FastScanner(final InputStream in) {
+            reader = new BufferedReader(new InputStreamReader(in));
+        }
+
+        String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+
+    public static void main(final String[] arg) {
+        final FastScanner in = new FastScanner(System.in);
+        try (PrintWriter out = new PrintWriter(System.out)) {
+            solve(in, out);
+        }
+    }
+}
