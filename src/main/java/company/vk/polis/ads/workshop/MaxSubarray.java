10bc5e9cea33e004368d72e0a78d94c5c30bd594
Kirill06344 <kirill12093@mail.ru>
2022-10-20 17:03:45 +0300

    add MaxSubArray

--- src/main/java/company/vk/polis/ads/workshop/MaxSubarray.java
+++ src/main/java/company/vk/polis/ads/workshop/MaxSubarray.java
@@ -13,14 +13,42 @@ public final class MaxSubarray {
             return new Res(l, r, array[l]);
         }
         int mid = (l + r) >> 1;
-        Res left = maxSubarray(array, l, mid);
-        Res right = maxSubarray(array, mid, r);
-        Res max = (left.sum > right.sum) ? left : right;
-        if (left.right == right.left) {
-            if (left.sum + right.sum > max.sum) {
-                max = new Res(left.left, right.right, left.sum + right.sum);
+        var left = maxSubarray(array, l, mid);
+        var right = maxSubarray(array, mid, r);
+        var cross = maxSubarrayCross(array, l, mid, r);
+        if (left.sum >= right.sum && left.sum >= cross.sum) {
+            return left;
+        } else if (right.sum >= left.sum && right.sum >= cross.sum) {
+            return right;
+        } else {
+            return cross;
+        }
+    }
+
+    private static Res maxSubarrayCross(int [] array,int l, int w, int r) {
+        int sum = 0;
+        int leftMaxSum = Integer.MIN_VALUE;
+        int leftPosition = 0;
+        for(int i = w - 1; i >= l; i--) {
+            sum += array[i];
+            if (sum > leftMaxSum) {
+                leftMaxSum = sum;
+                leftPosition = i;
             }
         }
-        return max;
+
+        sum = 0;
+        int rightMaxSum = Integer.MIN_VALUE;
+        int rightPosition = 0;
+        for(int i = w; i < r; i++) {
+            sum += array[i];
+            if (sum > rightMaxSum) {
+                rightMaxSum = sum;
+                rightPosition = i;
+            }
+        }
+
+        return new Res(leftPosition, rightPosition, leftMaxSum + rightMaxSum);
     }
 }
+
