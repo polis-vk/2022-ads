661499426fc43a4e04180324de053c0c824e879d
StombieIT <stombie@yandex.ru>
2022-10-20 15:51:42 +0300

    sorts done

---
+++ src/main/java/company/vk/polis/ads/Sorts.java
@@ -0,0 +1,152 @@
+package company.vk.polis.ads;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Sorts {
+
+    private static final Random random = new Random();
+
+    public static <T extends Comparable<T>> void insertionSort(T[] array) {
+        insertionSort(array, 0, array.length);
+    }
+
+    public static <T extends Comparable<T>> void insertionSort(T[] array, int fromInclusive,
+                                                               int toExclusive) {
+        for (int i = fromInclusive + 1; i < toExclusive; i++) {
+            int j = i;
+            while (j > 0 && array[j].compareTo(array[j - 1]) < 0) {
+                swap(array, j, j - 1);
+                j--;
+            }
+        }
+    }
+
+    public static <T extends Comparable<T>> void improvedInsertionSort(T[] array) {
+        improvedInsertionSort(array, 0, array.length);
+    }
+
+    public static <T extends Comparable<T>> void improvedInsertionSort(T[] array, int fromInclusive,
+                                                                       int toExclusive) {
+        for (int i = fromInclusive + 1; i < toExclusive; i++) {
+            T key = array[i];
+            if (key.compareTo(array[i - 1]) >= 0) {
+                continue;
+            }
+            int insertionPosition = insertionSearch(array, key, fromInclusive, i);
+            System.arraycopy(array, insertionPosition, array, insertionPosition + 1, i - insertionPosition);
+            array[insertionPosition] = key;
+        }
+    }
+
+    public static <T extends Comparable<T>> void heapSort(T[] array) {
+        int n = array.length - 1;
+        for (int i = n / 2; i >= 1; i--) {
+            sink(array, i, n);
+        }
+        for (int i = 0; i < n - 1; i++) {
+            swap(array, 1, n - i);
+            sink(array, 1, n - i - 1);
+        }
+    }
+
+    public static <T extends Comparable<T>> void mergeSort(T[] array) {
+        T[] tempArray = Arrays.copyOf(array, array.length);
+        mergeSort(array, tempArray, 0, array.length);
+    }
+
+    public static <T extends Comparable<T>> void mergeSort(T[] array, T[] tempArray,
+                                                           int fromInclusive, int toExclusive) {
+        if (fromInclusive >= toExclusive - 1) {
+            return;
+        }
+
+        int mid = fromInclusive + (toExclusive - fromInclusive) / 2;
+
+        mergeSort(array, tempArray, fromInclusive, mid);
+        mergeSort(array, tempArray, mid, toExclusive);
+
+        int i = fromInclusive;
+        int j = mid;
+        int t = fromInclusive;
+        while (i < mid && j < toExclusive) {
+            if (tempArray[i].compareTo(tempArray[j]) < 0) {
+                array[t++] = tempArray[i++];
+            } else {
+                array[t++] = tempArray[j++];
+            }
+        }
+
+        while (i < mid) {
+            array[t++] = tempArray[i++];
+        }
+
+        while (j < toExclusive) {
+            array[t++] = tempArray[j++];
+        }
+
+        System.arraycopy(array, 0, tempArray, 0, tempArray.length);
+    }
+
+    public static <T extends Comparable<T>> void quickSort(T[] array) {
+        quickSort(array, 0, array.length);
+    }
+
+    public static <T extends Comparable<T>> void quickSort(T[] array, int fromInclusive, int toExclusive) {
+        if (fromInclusive >= toExclusive - 1) {
+            return;
+        }
+        int partitionIndex = partition(array, fromInclusive, toExclusive);
+        quickSort(array, fromInclusive, partitionIndex);
+        quickSort(array, partitionIndex + 1, toExclusive);
+    }
+
+    private static <T extends Comparable<T>> int partition(T[] array, int fromInclusive, int toExclusive) {
+        swap(array, random.nextInt(fromInclusive, toExclusive), toExclusive - 1);
+        int i = fromInclusive - 1;
+        for (int j = fromInclusive; j < toExclusive; j++) {
+            if (array[j].compareTo(array[toExclusive - 1]) <= 0) {
+                swap(array, ++i, j);
+            }
+        }
+        return i;
+    }
+
+    private static <T extends Comparable<T>> void sink(T[] array, int k, int n) {
+        while (k * 2 <= n) {
+            int j = k * 2;
+            if (j < n && array[j].compareTo(array[j + 1]) < 0) {
+                j++;
+            }
+            if (array[k].compareTo(array[j]) >= 0) {
+                break;
+            }
+            swap(array, k, j);
+            k = j;
+        }
+    }
+
+    public static <T extends Comparable<T>> int insertionSearch(T[] array, T key,
+                                                                 int fromInclusive, int toExclusive) {
+        int l = fromInclusive;
+        int r = toExclusive;
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            int cmp = array[mid].compareTo(key);
+            if (cmp > 0) {
+                r = mid;
+            } else if (cmp < 0) {
+                l = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return l;
+    }
+
+    private static <T> void swap(T[] array, int i, int j) {
+        T temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
---
+++ src/test/java/company/vk/polis/ads/SortsTest.java
@@ -0,0 +1,86 @@
+package company.vk.polis.ads;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SortsTest {
+
+    private static final int DEFAULT_ARRAY_SIZE = 100_000;
+
+    @Test
+    public void insertionSortTest() {
+        Integer[] arr = generateRandomIntegerArray(DEFAULT_ARRAY_SIZE);
+
+        Integer[] arrCopy = Arrays.copyOf(arr, arr.length);
+
+        Sorts.insertionSort(arr);
+        Arrays.sort(arrCopy);
+
+        assertArraysEqual(arr, arrCopy);
+    }
+
+    @Test
+    public void improvedInsertionSort() {
+        Integer[] arr = generateRandomIntegerArray(DEFAULT_ARRAY_SIZE);
+        Integer[] arrCopy = Arrays.copyOf(arr, arr.length);
+
+        Sorts.improvedInsertionSort(arr);
+        Arrays.sort(arrCopy);
+
+        assertArraysEqual(arr, arrCopy);
+    }
+
+    @Test
+    public void heapSort() {
+        Integer[] arr = generateRandomIntegerArray(DEFAULT_ARRAY_SIZE);
+        arr[0] = Integer.MIN_VALUE;
+        Integer[] arrCopy = Arrays.copyOf(arr, arr.length);
+
+        Sorts.heapSort(arr);
+        Arrays.sort(arrCopy);
+
+        assertArraysEqual(arr, arrCopy);
+    }
+
+    @Test
+    public void mergeSort() {
+        Integer[] arr = generateRandomIntegerArray(DEFAULT_ARRAY_SIZE);
+        Integer[] arrCopy = Arrays.copyOf(arr, arr.length);
+
+        Sorts.mergeSort(arr);
+        Arrays.sort(arrCopy);
+
+        assertArraysEqual(arr, arrCopy);
+    }
+
+    @Test
+    public void quickSort() {
+        Integer[] arr = generateRandomIntegerArray(DEFAULT_ARRAY_SIZE);
+        Integer[] arrCopy = Arrays.copyOf(arr, arr.length);
+
+        Sorts.quickSort(arr);
+        Arrays.sort(arrCopy);
+
+        assertArraysEqual(arr, arrCopy);
+    }
+
+    private static Integer[] generateRandomIntegerArray(int size) {
+        Integer[] array = new Integer[size];
+        final Random random = new Random();
+        for (int i = 0; i < size; i++) {
+            array[i] = random.nextInt();
+        }
+        return array;
+    }
+
+    private static <T> void assertArraysEqual(T[] array1, T[] array2) {
+        assertEquals(array1.length, array2.length);
+        for (int i = 0; i < array1.length; i++) {
+            assertEquals(array1[i], array2[i]);
+        }
+    }
+}
