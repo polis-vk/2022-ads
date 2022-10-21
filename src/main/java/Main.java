b2c162a1ea7b7fb9bd50b3c53b9a176a2288b640
PopovIV <71888203+PopovIV@users.noreply.github.com>
2022-10-22 00:27:16 +0300

    Added insertion and circular buffer

---
+++ src/main/java/Main.java
@@ -0,0 +1,71 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+/**
+ * Problem solution template.
+ *
+ * @author Dmitry Schitinin
+ */
+public final class Main {
+    private Main() {
+        // Should not be instantiated
+    }
+
+    private static void solve(final FastScanner in, final PrintWriter out) {
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int[] len = new int[n];
+        int sum = 0;
+        for (int i = 0; i < len.length; i++) {
+            len[i] = in.nextInt();
+            sum += len[i];
+        }
+        int m = sum / k;
+        while (m != 0) {
+            sum = 0;
+            for (int e : len) {
+                sum += e / m;
+            }
+            if (sum == k) {
+                break;
+            }
+            m--;
+        }
+        out.println(m);
+    }
+
+    private static final class FastScanner {
+        private final BufferedReader reader;
+        private StringTokenizer tokenizer;
+
+        FastScanner(final InputStream in) {
+            reader = new BufferedReader(new InputStreamReader(in));
+        }
+
+        String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+
+    public static void main(final String[] arg) {
+        final FastScanner in = new FastScanner(System.in);
+        try (PrintWriter out = new PrintWriter(System.out)) {
+            solve(in, out);
+        }
+    }
+}
