91352098d867802ba6d3062f6a4c131696d0b38b
AlexAndrovSPBSTU <AleksandrovAlexey0929@yandex.ru>
2022-10-24 18:50:54 +0300

    CircularBufferQueue is done

--- src/main/java/company/vk/polis/ads/workshop/CircularBufferQueue.java
+++ src/main/java/company/vk/polis/ads/workshop/CircularBufferQueue.java
@@ -2,7 +2,10 @@ package company.vk.polis.ads.workshop;
 
 import java.util.AbstractQueue;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 import java.util.Queue;
+
+import javax.print.DocFlavor;
 
 /**
  * Queue implementation based on fixed size circular buffer.
@@ -10,34 +13,72 @@ import java.util.Queue;
  * @param <E> type of elements
  */
 public final class CircularBufferQueue<E> extends AbstractQueue<E> implements Queue<E> {
+    private final Object[] array;
+    private int size;
+    private int head;
+    private int tail;
+
     public CircularBufferQueue(int maxCapacity) {
-        throw new UnsupportedOperationException();
+        array = new Object[maxCapacity];
     }
 
     @Override
     public Iterator<E> iterator() {
-        throw new UnsupportedOperationException();
+        return new Iterator<E>() {
+            int currentIndex = head;
+
+            @Override
+            public boolean hasNext() {
+                return currentIndex < tail && size != 0 ;
+            }
+
+            @Override
+            public E next() {
+                if (!hasNext()){
+                    throw new NoSuchElementException();
+                }
+                final var el =  (E) array[currentIndex];
+                currentIndex++;
+                currentIndex %= array.length;
+                return el;
+            }
+        };
     }
 
     @Override
     public int size() {
-        throw new UnsupportedOperationException();
+        return size;
     }
 
     @Override
     public boolean offer(E e) {
-        throw new UnsupportedOperationException();
-
+        if (size == array.length) {
+            return false;
+        }
+        size++;
+        array[tail] = e;
+        tail++;
+        tail %= array.length;
+        return true;
     }
 
     @Override
     public E poll() {
-        throw new UnsupportedOperationException();
-
+        if (isEmpty()) {
+            return null;
+        }
+        final var el = array[head];
+        head++;
+        head %= array.length;
+        size--;
+        return (E) el;
     }
 
     @Override
     public E peek() {
-        throw new UnsupportedOperationException();
+        if (isEmpty()) {
+            return null;
+        }
+        return (E) array[head];
     }
 }
