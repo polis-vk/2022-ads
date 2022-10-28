8950eccea69f92731db0050272abc37cb422fa07
Queenore <atcheschev@yandex.ru>
2022-10-28 16:15:32 +0300

    ImprovedInsertionSort.java and MaxSubarray.java done

--- src/main/java/company/vk/polis/ads/workshop/ImprovedInsertionSort.java
+++ src/main/java/company/vk/polis/ads/workshop/ImprovedInsertionSort.java
@@ -6,6 +6,32 @@ public final class ImprovedInsertionSort {
     }
 
     public static <E extends Comparable<E>> void sort(E[] array, int fromInclusive, int toExclusive) {
-        throw new UnsupportedOperationException();
+        for (int i = fromInclusive + 1; i < toExclusive; i++) {
+            E key = array[i];
+            if (key.compareTo(array[i - 1]) >= 0) {
+                continue;
+            }
+            int insertPos = insertionPosition(array, array[i], fromInclusive, i);
+            System.arraycopy(array, insertPos, array, insertPos + 1, i - insertPos);
+            array[insertPos] = key;
+        }
+    }
+
+    private static <E extends Comparable<E>> int insertionPosition(E[] array, E key, int fromInclusive, int toExclusive) {
+        int left = fromInclusive;
+        int right = toExclusive;
+        while (left < right) {
+            int mid = (right + left) >> 1;
+            E elem = array[mid];
+            int cmp = elem.compareTo(key);
+            if (cmp > 0) {
+                right = mid;
+            } else if (cmp < 0) {
+                left = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return left;
     }
 }
