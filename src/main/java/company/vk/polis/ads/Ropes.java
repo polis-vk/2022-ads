69a56c0f36e42cb69c361c2dfe95021517dd1d93
Vladimir <i.am.polshin@gmail.com>
2022-10-29 18:07:24 +0300

    Ropes: all done

--- src/main/java/company/vk/polis/ads/Ropes.java
+++ src/main/java/company/vk/polis/ads/Ropes.java
@@ -12,13 +12,42 @@ import java.util.StringTokenizer;
  *
  * @author Dmitry Schitinin
  */
-public final class SolveTemplate {
-    private SolveTemplate() {
+public final class Ropes {
+    private Ropes() {
         // Should not be instantiated
     }
 
+    //Решение: https://www.eolymp.com/ru/submissions/11978685
     private static void solve(final FastScanner in, final PrintWriter out) {
-        // Write me
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int[] ropes = new int[n];
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            int rope = in.nextInt();
+            ropes[i] = rope;
+            if (max < rope) {
+                max = rope;
+            }
+        }
+
+        int l = 0;
+        int r = max + 1;
+
+        while (l < r - 1) {
+            int mid = (l + r) >>> 1;
+            int houseCount = 0;
+            for (int length : ropes) {
+                int cnt = length / mid;
+                houseCount += cnt;
+            }
+            if (houseCount < k) {
+                r = mid;
+            } else {
+                l = mid;
+            }
+        }
+        out.println(l);
     }
 
     private static final class FastScanner {
