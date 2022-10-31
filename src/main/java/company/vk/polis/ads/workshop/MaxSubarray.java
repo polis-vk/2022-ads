3b825aea455b0f46305138d32f983d31a0e59931
NIKITry <nikitabandurin2002@gmail.com>
2022-10-31 19:08:04 +0300

    add MaxSubarray

--- src/main/java/company/vk/polis/ads/workshop/MaxSubarray.java
+++ src/main/java/company/vk/polis/ads/workshop/MaxSubarray.java
@@ -9,6 +9,42 @@ public final class MaxSubarray {
     }
 
     private static Res maxSubarray(int[] array, int l, int r) {
-        throw new UnsupportedOperationException();
+        if (l == r - 1) {
+            return new Res(l, r, array[l]);
+        }
+        int mid = (l + r) >>> 1;
+        var left = maxSubarray(array, l, mid);
+        var right = maxSubarray(array, mid, r);
+        var cross = maxSubarrayCross(array, mid, l, r);
+        if (left.sum >= right.sum && left.sum >= cross.sum) {
+            return left;
+        } else if (right.sum >= left.sum && right.sum >= cross.sum) {
+            return right;
+        }
+        return cross;
+    }
+
+    private static Res maxSubarrayCross(int[] array, int mid, int l, int r) {
+        int sum = 0;
+        int leftMaxSum = Integer.MIN_VALUE;
+        int leftPosition = mid;
+        for (int i = mid - 1; i >= l; i--) {
+            sum += array[i];
+            if (sum >= leftMaxSum) {
+                leftMaxSum = sum;
+                leftPosition = i;
+            }
+        }
+        sum = 0;
+        int rightMaxSum = Integer.MIN_VALUE;
+        int rightPosition = mid - 1;
+        for (int i = mid; i < r; i++) {
+            sum += array[i];
+            if (sum > rightMaxSum) {
+                rightMaxSum = sum;
+                rightPosition = i;
+            }
+        }
+        return new Res(leftPosition, rightPosition, leftMaxSum + rightMaxSum);
     }
 }
