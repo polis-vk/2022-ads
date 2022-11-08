bfaa6073ab3e2513a910b91ee17e6cec5a13571c
Ekaterina Zelenkina <katrinzel0@mail.ru>
2022-11-08 15:55:46 +0300

    AvlBinarySearchTree done

--- src/main/java/company/vk/polis/ads/bst/AvlBinarySearchTree.java
+++ src/main/java/company/vk/polis/ads/bst/AvlBinarySearchTree.java
@@ -7,66 +7,273 @@ import org.jetbrains.annotations.NotNull;
  */
 public class AvlBinarySearchTree<Key extends Comparable<Key>, Value> implements BinarySearchTree<Key, Value> {
 
+    private static final int BALANCE_FACTOR_VALUE = 2;
+    private Node lastRemoved;
+    private Node root;
+    private int size;
+
     private class Node {
         Key key;
         Value value;
         Node left;
         Node right;
         int height;
+
+        Node(Key key, Value value, int height) {
+            this.key = key;
+            this.value = value;
+            this.height = height;
+        }
     }
 
     @Override
     public Value get(@NotNull Key key) {
-        throw new UnsupportedOperationException("Implement me");
+        return get(root, key);
     }
 
     @Override
     public void put(@NotNull Key key, @NotNull Value value) {
-        throw new UnsupportedOperationException("Implement me");
+        root = put(root, key, value);
     }
 
     @Override
     public Value remove(@NotNull Key key) {
-        throw new UnsupportedOperationException("Implement me");
+        root = delete(root, key);
+        if (lastRemoved != null){
+            Value deleteValue = lastRemoved.value;
+            lastRemoved = null;
+            size--;
+            return deleteValue;
+        }
+        return null;
     }
 
     @Override
     public Key min() {
-        throw new UnsupportedOperationException("Implement me");
+        if (root == null) {
+            return null;
+        }
+        return min(root).key;
     }
 
     @Override
     public Value minValue() {
-        throw new UnsupportedOperationException("Implement me");
+        if (root == null) {
+            return null;
+        }
+        return min(root).value;
     }
 
     @Override
     public Key max() {
-        throw new UnsupportedOperationException("Implement me");
+        if (root == null) {
+            return null;
+        }
+        return max(root).key;
     }
 
     @Override
     public Value maxValue() {
-        throw new UnsupportedOperationException("Implement me");
+        if (root == null) {
+            return null;
+        }
+        return max(root).value;
     }
 
     @Override
     public Key floor(@NotNull Key key) {
-        throw new UnsupportedOperationException("Implement me");
+        Node floorNode = floor(root, key);
+        if (floorNode == null) {
+            return null;
+        }
+        return floorNode.key;
     }
 
     @Override
     public Key ceil(@NotNull Key key) {
-        throw new UnsupportedOperationException("Implement me");
+        Node ceilNode = ceil(root, key);
+        if (ceilNode == null) {
+            return null;
+        }
+        return ceilNode.key;
     }
 
     @Override
     public int size() {
-        throw new UnsupportedOperationException("Implement me");
+        return size;
     }
 
     @Override
     public int height() {
-        throw new UnsupportedOperationException("Implement me");
+        return height(root);
     }
+
+    private Value get(Node node, Key key) {
+        if (node == null) {
+            return null;
+        }
+        int compareResult = key.compareTo(node.key);
+        if (compareResult < 0) {
+            return get(node.left, key);
+        }
+        if (compareResult > 0) {
+            return get(node.right, key);
+        }
+        return node.value;
+    }
+
+    private Node put(Node node, Key key, Value value) {
+        if (node == null) {
+            size++;
+            return new Node(key, value, 1);
+        }
+        int compareResult = key.compareTo(node.key);
+        if (compareResult < 0) {
+            node.left = put(node.left, key, value);
+        } else if (compareResult > 0) {
+            node.right = put(node.right, key, value);
+        } else {
+            node.value = value;
+        }
+        fixHeight(node);
+        return balance(node);
+    }
+
+    private Node delete(Node node, Key key) {
+        if (node == null) {
+            return null;
+        }
+        int compareResult = key.compareTo(node.key);
+        if (compareResult < 0) {
+            node.left = delete(node.left, key);
+        } else if (compareResult > 0) {
+            node.right = delete(node.right, key);
+        } else {
+            lastRemoved = node;
+            node = innerDelete(node);
+        }
+        return node;
+    }
+
+    private Node innerDelete(Node node) {
+        if (node.right == null) {
+            return node.left;
+        }
+        if (node.left == null) {
+            return node.right;
+        }
+        Node tempNode = node;
+        node = min(node.right);
+        node.right = deleteMin(tempNode.right);
+        node.left = tempNode.left;
+        return node;
+    }
+
+    private Node deleteMin(Node node) {
+        if (node.left == null) {
+            return node.right;
+        }
+        node.left = deleteMin(node.left);
+        return node;
+    }
+
+    private Node min(Node node) {
+        if (node.left == null) {
+            return node;
+        }
+        return min(node.left);
+    }
+
+    private Node max(Node node) {
+        if (node.right == null) {
+            return node;
+        }
+        return max(node.right);
+    }
+
+    private Node floor(Node node, Key key) {
+        if (node == null) {
+            return null;
+        }
+        int compareResult = key.compareTo(node.key);
+        if (compareResult < 0) {
+            return floor(node.left, key);
+        }
+        if (compareResult == 0) {
+            return node;
+        }
+        Node nextNode = floor(node.right, key);
+        if (nextNode != null) {
+            return nextNode;
+        }
+        return node;
+    }
+
+    private Node ceil(Node node, Key key) {
+        if (node == null) {
+            return null;
+        }
+        int compareResult = key.compareTo(node.key);
+        if (compareResult > 0) {
+            return ceil(node.right, key);
+        }
+        if (compareResult == 0) {
+            return node;
+        }
+        Node nextNode = ceil(node.left, key);
+        if (nextNode != null) {
+            return nextNode;
+        }
+        return node;
+    }
+
+    private int height(Node node) {
+        if (node == null) {
+            return 0;
+        }
+        return node.height;
+    }
+
+    private void fixHeight(Node node) {
+        node.height = 1 + Math.max(height(node.left), height(node.right));
+    }
+
+    private int balanceFactor(Node node) {
+        return height(node.left) - height(node.right);
+    }
+
+    Node rotateLeft(Node node) {
+        Node rightNode = node.right;
+        node.right = rightNode.left;
+        rightNode.left = node;
+        fixHeight(node);
+        fixHeight(rightNode);
+        return rightNode;
+    }
+
+    Node rotateRight(Node node) {
+        Node leftNode = node.left;
+        node.left = leftNode.right;
+        leftNode.right = node;
+        fixHeight(node);
+        fixHeight(leftNode);
+        return leftNode;
+    }
+
+    private Node balance(Node node) {
+        if (balanceFactor(node) == BALANCE_FACTOR_VALUE) {
+            if (balanceFactor(node.left) < 0) {
+                node.left = rotateLeft(node.left);
+            }
+            return rotateRight(node);
+        }
+        if (balanceFactor(node) == -BALANCE_FACTOR_VALUE) {
+            if (balanceFactor(node.right) > 0) {
+                node.right = rotateRight(node.right);
+            }
+            return rotateLeft(node);
+        }
+        return node;
+    }
+
 }
