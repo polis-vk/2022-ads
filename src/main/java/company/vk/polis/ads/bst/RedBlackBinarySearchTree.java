1d3174e77f7e1be03615bc775ce2f5f858409ef7
AlexAndrovSPBSTU <AleksandrovAlexey0929@yandex.ru>
2022-11-15 00:31:28 +0300

    RedBlackBinarySearchTree is done

--- src/main/java/company/vk/polis/ads/bst/RedBlackBinarySearchTree.java
+++ src/main/java/company/vk/polis/ads/bst/RedBlackBinarySearchTree.java
@@ -11,6 +11,8 @@ public class RedBlackBinarySearchTree<Key extends Comparable<Key>, Value>
 
     private static final boolean BLACK = false;
     private static final boolean RED = true;
+    private Node root;
+    private int size;
 
     private class Node {
         Key key;
@@ -18,65 +20,293 @@ public class RedBlackBinarySearchTree<Key extends Comparable<Key>, Value>
         Node left;
         Node right;
         boolean color;
+
+        public Node(Key key, Value value, boolean color) {
+            this.key = key;
+            this.value = value;
+            this.color = color;
+        }
     }
 
     @Nullable
     @Override
     public Value get(@NotNull Key key) {
-        throw new UnsupportedOperationException("Implement me");
+        return get(root, key);
+    }
+
+    private Value get(Node x, Key key) {
+        if (x == null) {
+            return null;
+        }
+        if (key.compareTo(x.key) < 0) {
+            return get(x.left, key);
+        }
+        if (key.compareTo(x.key) > 0) {
+            return get(x.right, key);
+        }
+        return x.value;
     }
 
     @Override
     public void put(@NotNull Key key, @NotNull Value value) {
-        throw new UnsupportedOperationException("Implement me");
+        root = put(root, key, value);
+        root.color = BLACK;
+    }
+
+    Node put(Node x, Key key, Value value) {
+        if (x == null) {
+            size++;
+            return new Node(key, value, RED);
+        }
+        if (key.compareTo(x.key) < 0) {
+            x.left = put(x.left, key, value);
+        } else if (key.compareTo(x.key) > 0) {
+            x.right = put(x.right, key, value);
+        } else {
+            x.value = value;
+        }
+        return fixUp(x);
     }
 
     @Nullable
     @Override
     public Value remove(@NotNull Key key) {
-        throw new UnsupportedOperationException("Implement me");
+        Value valueOfRemoved = get(key);
+        if (valueOfRemoved != null) {
+            size--;
+            root = remove(root,key);
+            return valueOfRemoved;
+        } else {
+            return null;
+        }
+    }
+
+
+    private Node remove(Node x, Key key) {
+        if (x == null) {
+            return null;
+        }
+        if (key.compareTo(x.key) < 0) {
+            if (x.left != null) {
+                if (!isRed(x.left) && !isRed(x.left.left)) {
+                    x = moveRedLeft(x);
+                }
+                x.left = remove(x.left, key);
+            }
+        } else {
+            if (isRed(x.left)) {
+                x = rotateRight(x);
+                x.right = remove(x.right, key);
+            } else if (x.key == key && x.right == null) {
+                return null;
+            } else {
+                if (x.right != null && !isRed(x.right) && !isRed(x.right.left)) {
+                    x = moveRedRight(x);
+                }
+                if (x.key == key) {
+                    Node min = minNode(x.right);
+                    x.key = min.key;
+                    x.value = min.value;
+                    x.right = deleteMin(x.right);
+                } else {
+                    x.right = remove(x.right, key);
+                }
+            }
+        }
+        return fixUp(x);
     }
 
     @Nullable
     @Override
     public Key min() {
-        throw new UnsupportedOperationException("Implement me");
+        Node minNode = minNode(root);
+        return minNode == null ? null : minNode.key;
     }
 
     @Nullable
     @Override
     public Value minValue() {
-        throw new UnsupportedOperationException("Implement me");
+        Node minNode = minNode(root);
+        return minNode == null ? null : minNode.value;
+    }
+
+    private Node minNode(Node node) {
+        if (node == null) {
+            return null;
+        }
+        while (node.left != null) {
+            node = node.left;
+        }
+        return node;
     }
 
     @Nullable
     @Override
     public Key max() {
-        throw new UnsupportedOperationException("Implement me");
+        Node maxNode = maxNode(root);
+        return maxNode == null ? null : maxNode.key;
     }
 
     @Nullable
     @Override
     public Value maxValue() {
-        throw new UnsupportedOperationException("Implement me");
+        Node maxNode = maxNode(root);
+        return maxNode == null ? null : maxNode.value;
+    }
+
+    private Node maxNode(Node node) {
+        if (node == null) {
+            return null;
+        }
+        while (node.right != null) {
+            node = node.right;
+        }
+        return node;
     }
 
     @Nullable
     @Override
     public Key floor(@NotNull Key key) {
-        throw new UnsupportedOperationException("Implement me");
+        return searchFloor(key, root);
+    }
+
+    private Key searchFloor(Key key, Node current) {
+        if (current == null) {
+            return null;
+        }
+        if (current.key.compareTo(key) == 0) {
+            return current.key;
+        }
+        if (current.key.compareTo(key) > 0) {
+            return searchFloor(key, current.left);
+        } else {
+            Key floor = searchFloor(key, current.right);
+            return (floor != null && floor.compareTo(key) <= 0) ? floor : current.key;
+        }
     }
 
     @Nullable
     @Override
     public Key ceil(@NotNull Key key) {
-        throw new UnsupportedOperationException("Implement me");
+        return searchCeil(key, root);
+    }
+
+    private Key searchCeil(Key key, Node current) {
+        if (current == null) {
+            return null;
+        }
+        if (current.key.compareTo(key) == 0) {
+            return current.key;
+        }
+        if (current.key.compareTo(key) < 0) {
+            return searchCeil(key, current.right);
+        }
+        Key ceil = searchCeil(key, current.left);
+        return (ceil != null && ceil.compareTo(key) >= 0) ? ceil : current.key;
     }
 
     @Override
     public int size() {
-        throw new UnsupportedOperationException("Implement me");
+        return size;
     }
+
+    private Node moveRedRight(Node x) {
+        flipColors(x);
+        if (isRed(x.left.left)) {
+            x = rotateRight(x);
+            flipColors(x);
+        }
+        return x;
+    }
+
+    void deleteMax() {
+        root = deleteMax(root);
+        root.color = BLACK;
+    }
+
+    Node deleteMax(Node x) {
+        if (isRed(x.left)) {
+            x = rotateRight(x);
+        }
+        if (x.right == null) {
+            return null;
+        }
+        if (!isRed(x.right) && !isRed(x.right.left)) {
+            x = moveRedRight(x);
+        }
+        x.right = deleteMax(x.right);
+        return fixUp(x);
+    }
+
+    private Node deleteMin(Node x) {
+        if (x.left == null) {
+            return null;
+        }
+        if (!isRed(x.left) && !isRed(x.left.left)) {
+            x = moveRedLeft(x);
+        }
+        x.left = deleteMin(x.left);
+        return fixUp(x);
+    }
+
+    void deleteMin() {
+        root = deleteMin(root);
+        root.color = BLACK;
+    }
+
+    private Node moveRedLeft(Node x) {
+        flipColors(x);
+        if (isRed(x.right.left)) {
+            x.right = rotateRight(x.right);
+            x = rotateLeft(x);
+            flipColors(x);
+        }
+        return x;
+    }
+
+
+    private boolean isRed(Node x) {
+        return x != null && x.color == RED;
+    }
+
+    private Node fixUp(Node x) {
+        if (isRed(x.right) && !isRed(x.left)) {
+            x = rotateLeft(x);
+        }
+        if (isRed(x.left) && isRed(x.left.left)) {
+            x = rotateRight(x);
+        }
+        if (isRed(x.left) && isRed(x.right)) {
+            flipColors(x);
+        }
+        return x;
+    }
+
+    private Node rotateLeft(Node x) {
+        Node y = x.right;
+        x.right = y.left;
+        y.left = x;
+        y.color = x.color;
+        x.color = RED;
+        return y;
+    }
+
+    private Node rotateRight(Node y) {
+        Node x = y.left;
+        y.left = x.right;
+        x.right = y;
+        x.color = y.color;
+        y.color = RED;
+        return x;
+    }
+
+    private Node flipColors(Node x) {
+        x.color = !x.color;
+        x.left.color = !x.left.color;
+        x.right.color = !x.right.color;
+        return x;
+    }
+
 
     /**
      * Только для тестов
@@ -84,6 +314,10 @@ public class RedBlackBinarySearchTree<Key extends Comparable<Key>, Value>
      */
     @Override
     public int height() {
-        throw new UnsupportedOperationException("Implement me");
+        return height(root);
+    }
+
+    private int height(Node x) {
+        return (x == null) ? 0 : Math.max(height(x.left), height(x.right)) + 1;
     }
 }
