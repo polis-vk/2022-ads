a0f5f2b7ae146a113ab2df8c4656dfe1b1c11b4d
Vladislav Pochernin <vspochernin@gmail.com>
2022-11-19 16:23:45 +0300

    part8-DoubleHashingMap: implement

--- src/main/java/company/vk/polis/ads/hash/DoubleHashingMap.java
+++ src/main/java/company/vk/polis/ads/hash/DoubleHashingMap.java
@@ -15,6 +15,9 @@ public final class DoubleHashingMap<K, V> implements Map<K, V> {
     private K[] keys;
     private V[] values;
     private boolean[] removed;
+    private int capacity;
+    private int size = 0;
+    private final float loadFactor;
 
     /**
      * Создает новый ассоциативный массив в соответствии с expectedMaxSize и loadFactor.
@@ -27,25 +30,45 @@ public final class DoubleHashingMap<K, V> implements Map<K, V> {
      * @param loadFactor      отношение количества элементов к размеру массивов
      */
     public DoubleHashingMap(int expectedMaxSize, float loadFactor) {
-        keys = allocate(0);
-        values = allocate(0);
-        removed = new boolean[0];
+        this.loadFactor = loadFactor;
+        this.capacity = (int) (expectedMaxSize / loadFactor);
+        this.keys = allocate(capacity);
+        this.values = allocate(capacity);
+        this.removed = new boolean[capacity];
     }
 
     @Override
     public int size() {
-        throw new UnsupportedOperationException();
+        return size;
     }
 
     @Override
     public boolean containsKey(K key) {
-        throw new UnsupportedOperationException();
+        int hash = hash(key);
+        int hash2 = hash2(key);
+        int i = 0;
+
+        for (int index = getIndex(hash, hash2, i++); keys[index] != null; index = getIndex(hash, hash2, i++)) {
+            if (!removed[index] && keys[index].equals(key)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     @Nullable
     @Override
     public V get(K key) {
-        throw new UnsupportedOperationException();
+        int hash = hash(key);
+        int hash2 = hash2(key);
+        int i = 0;
+
+        for (int index = getIndex(hash, hash2, i++); keys[index] != null; index = getIndex(hash, hash2, i++)) {
+            if (!removed[index] && keys[index].equals(key)) {
+                return values[index];
+            }
+        }
+        return null;
     }
 
     /**
@@ -55,22 +78,115 @@ public final class DoubleHashingMap<K, V> implements Map<K, V> {
     @Nullable
     @Override
     public V put(K key, V value) {
-        throw new UnsupportedOperationException();
+        resizeIfNeed();
+
+        int hash = hash(key);
+        int hash2 = hash2(key);
+        int i = 0;
+
+        int index;
+        for (index = getIndex(hash, hash2, i++); keys[index] != null; index = getIndex(hash, hash2, i++)) {
+            if (!removed[index] && keys[index].equals(key)) {
+                V result = values[index];
+                values[index] = value;
+                return result;
+            }
+        }
+
+        size++;
+        keys[index] = key;
+        values[index] = value;
+        removed[index] = false;
+        return null;
     }
 
     @Nullable
     @Override
     public V remove(K key) {
-        throw new UnsupportedOperationException();
+        int hash = hash(key);
+        int hash2 = hash2(key);
+        int i = 0;
+
+        for (int index = getIndex(hash, hash2, i++); keys[index] != null; index = getIndex(hash, hash2, i++)) {
+            if (!removed[index] && keys[index].equals(key)) {
+                size--;
+                removed[index] = true;
+                return values[index];
+            }
+        }
+        return null;
     }
 
     @Override
     public void forEach(BiConsumer<K, V> consumer) {
-        throw new UnsupportedOperationException();
+        for (int i = 0; i < capacity; i++) {
+            if (!removed[i] && keys[i] != null) {
+                consumer.accept(keys[i], values[i]);
+            }
+        }
     }
 
     @SuppressWarnings("unchecked")
     private static <T> T[] allocate(int capacity) {
         return (T[]) new Object[capacity];
+    }
+
+    private int hash(K key) {
+        return key.hashCode();
+    }
+
+    private int hash2(K key) {
+        return key.hashCode();
+    }
+
+    private int getIndex(int hash, int hash2, int i) {
+        return ((hash + i * hash2) & 0x7fffffff) % capacity;
+    }
+
+    private void resizeIfNeed() {
+        if (capacity * loadFactor > size) {
+            return;
+        }
+
+        int oldCapacity = capacity;
+        K[] oldKeys = keys;
+        V[] oldValues = values;
+
+        capacity = getNextPrime(capacity * 2);
+        size = 0;
+        keys = allocate(capacity);
+        values = allocate(capacity);
+        removed = new boolean[capacity];
+
+        for (int i = 0; i < oldCapacity; i++) {
+            if (oldKeys[i] != null) {
+                put(oldKeys[i], oldValues[i]);
+            }
+        }
+    }
+
+    private int getNextPrime(int n) {
+        int curNum = n;
+        while (true) {
+            if (isPrime(curNum)) {
+                return curNum;
+            }
+            curNum++;
+        }
+    }
+
+    private boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+
+        int limit = (int) Math.sqrt(n);
+        for (int i = 2; i < limit; i += 1) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
