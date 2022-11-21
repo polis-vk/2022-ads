ddef3add112825a22579efdca91540c74313af86
osokindm <osokin34@gmail.com>
2022-11-21 22:20:45 +0300

    Osokin Dmitry 8th hw

--- src/main/java/company/vk/polis/ads/hash/DoubleHashingMap.java
+++ src/main/java/company/vk/polis/ads/hash/DoubleHashingMap.java
@@ -16,6 +16,10 @@ public final class DoubleHashingMap<K, V> implements Map<K, V> {
     private V[] values;
     private boolean[] removed;
 
+    private int expectedMaxSize; // threshold
+    private int size;
+    private int capacity;
+
     /**
      * Создает новый ассоциативный массив в соответствии с expectedMaxSize и loadFactor.
      * Сразу выделяет начальное количество памяти на основе expectedMaxSize и loadFactor.
@@ -27,25 +31,39 @@ public final class DoubleHashingMap<K, V> implements Map<K, V> {
      * @param loadFactor      отношение количества элементов к размеру массивов
      */
     public DoubleHashingMap(int expectedMaxSize, float loadFactor) {
-        keys = allocate(0);
-        values = allocate(0);
-        removed = new boolean[0];
+        this.expectedMaxSize = expectedMaxSize;
+        size = 0;
+        capacity = (int) (expectedMaxSize / loadFactor);
+        keys = allocate(capacity);
+        values = allocate(capacity);
+        removed = new boolean[capacity];
     }
 
     @Override
     public int size() {
-        throw new UnsupportedOperationException();
+        return size;
     }
 
     @Override
     public boolean containsKey(K key) {
-        throw new UnsupportedOperationException();
+        return get(key) != null;
     }
 
     @Nullable
     @Override
     public V get(K key) {
-        throw new UnsupportedOperationException();
+        int j = 0;
+        int i = indexOf(key, j++);
+        while (keys[i] != null) {
+            if (keys[i].equals(key)) {
+                if (removed[i]) {
+                    return null;
+                }
+                return values[i];
+            }
+            i = indexOf(key, j++);
+        }
+        return null;
     }
 
     /**
@@ -55,22 +73,115 @@ public final class DoubleHashingMap<K, V> implements Map<K, V> {
     @Nullable
     @Override
     public V put(K key, V value) {
-        throw new UnsupportedOperationException();
+        if (size + 1 > expectedMaxSize) {
+            resize();
+        }
+
+        int j = 0;
+        int i = indexOf(key, j++);
+        while (!removed[i] && keys[i] != null) {
+            if (keys[i].equals(key)) {
+                var oldValue = values[i];
+                values[i] = value;
+                return oldValue;
+            }
+            i = indexOf(key, j++);
+        }
+        keys[i] = key;
+        values[i] = value;
+        removed[i] = false;
+        size++;
+        return null;
     }
 
     @Nullable
     @Override
     public V remove(K key) {
-        throw new UnsupportedOperationException();
+        int j = 0;
+        int i = indexOf(key, j++);
+        while (keys[i] != null) {
+            if (keys[i].equals(key)) {
+                if (removed[i]) {
+                    return null;
+                }
+                removed[i] = true;
+                size--;
+                return values[i];
+            }
+            i = indexOf(key, j++);
+        }
+        return null;
     }
 
     @Override
     public void forEach(BiConsumer<K, V> consumer) {
-        throw new UnsupportedOperationException();
+        for (int i = 0; i < capacity; i++) {
+            if (keys[i] != null && !removed[i]) {
+                consumer.accept(keys[i], values[i]);
+            }
+        }
     }
 
     @SuppressWarnings("unchecked")
     private static <T> T[] allocate(int capacity) {
         return (T[]) new Object[capacity];
+    }
+
+    private int hash1(K key) {
+        int h;
+        return (key == null) ? 0 : (h = key.hashCode()) ^ (h >>> 16);
+    }
+
+    private int hash2(K key) {
+        long sum = 1;
+        long mult = 1;
+        int i = key.toString().length();
+        mult = (i % 4 == 0) ? 1 : mult * 256;
+        sum += key.hashCode() * mult;
+
+        return (int) sum;
+    }
+
+    private void resize() {
+        int oldCapacity = capacity;
+        var oldKeys = keys;
+        var oldValues = values;
+
+        capacity = generatePrime(capacity * 2);
+        expectedMaxSize = capacity;
+        keys = allocate(capacity);
+        values = allocate(capacity);
+        removed = new boolean[capacity];
+        size = 0;
+
+        for (int i = 0; i < oldCapacity; i++) {
+            if (oldKeys[i] != null) {
+                put(oldKeys[i], oldValues[i]);
+            }
+        }
+    }
+
+    private int indexOf(K key, int i) {
+        return Math.abs((hash1(key) + i * hash2(key)) % capacity);
+    }
+
+    //generates nearest greater prime number
+    private int generatePrime(int n) {
+        int pr;
+        for (pr = n; pr < Integer.MAX_VALUE; pr++) {
+            if (isPrime(pr)) {
+                break;
+            }
+        }
+        return pr;
+    }
+
+    private boolean isPrime(int n) {
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- src/main/java/company/vk/polis/ads/hash/SeparateChainingMap.java
+++ src/main/java/company/vk/polis/ads/hash/SeparateChainingMap.java
@@ -1,5 +1,6 @@
 package company.vk.polis.ads.hash;
 
+import java.util.Objects;
 import java.util.function.BiConsumer;
 
 import org.jetbrains.annotations.Nullable;
@@ -11,8 +12,10 @@ import org.jetbrains.annotations.Nullable;
  * @param <V> value
  */
 public final class SeparateChainingMap<K, V> implements Map<K, V> {
-    // Do not edit this field!!!
     private Node<K, V>[] array;
+    private int expectedMaxSize; // threshold
+    private int size;
+    private int capacity; // количество элементов array; увеличиваем, когда больше expectedMaxSize
 
     /**
      * Создает новый ассоциативный массив в соответствии с expectedMaxSize и loadFactor.
@@ -25,24 +28,27 @@ public final class SeparateChainingMap<K, V> implements Map<K, V> {
      * @param loadFactor      отношение количества элементов к размеру массива связных списков
      */
     public SeparateChainingMap(int expectedMaxSize, float loadFactor) {
-        array = allocate(0);
-        throw new UnsupportedOperationException();
+        this.expectedMaxSize = expectedMaxSize;
+        size = 0;
+        capacity = (int) (expectedMaxSize / loadFactor);
+        array = allocate(capacity);
     }
 
     @Override
     public int size() {
-        throw new UnsupportedOperationException();
+        return size;
     }
 
     @Override
     public boolean containsKey(K key) {
-        throw new UnsupportedOperationException();
+        return getNode(key) != null;
     }
 
     @Nullable
     @Override
     public V get(K key) {
-        throw new UnsupportedOperationException();
+        var node = getNode(key);
+        return node == null ? null : node.value;
     }
 
     /**
@@ -52,18 +58,71 @@ public final class SeparateChainingMap<K, V> implements Map<K, V> {
     @Nullable
     @Override
     public V put(K key, V value) {
-        throw new UnsupportedOperationException();
+        if (size + 1 > expectedMaxSize) {
+            resize();
+        }
+        int index = indexOf(hash(key), capacity);
+        if (array[index] == null) {
+            array[index] = new Node<>(key, value);
+        } else {
+            Node<K, V> node = array[index];
+            Node<K, V> prevNode = null;
+            while (node != null) {
+                if (node.key.equals(key)) {
+                    V oldValue = node.value;
+                    node.value = value;
+                    return oldValue;
+                }
+                prevNode = node;
+                node = node.next;
+            }
+            node = new Node<>(key, value);
+            node.prev = prevNode;
+            node.prev.next = node;
+        }
+        size++;
+        return null;
     }
 
     @Nullable
     @Override
     public V remove(K key) {
-        throw new UnsupportedOperationException();
+        int index = indexOf(hash(key), capacity);
+        Node<K, V> node = array[index];
+
+        while (node != null) {
+            if (node.key.equals(key)) {
+                if (node.equals(array[index])) {
+                    // deleting the first element
+                    node.next.prev = null;
+                    array[index] = node.next;
+                } else {
+                    if (node.next != null) {
+                        node.next.prev = node.prev;
+                    }
+                    node.prev.next = node.next;
+                }
+                V value = node.value;
+                size--;
+                return value;
+            }
+            node = node.next;
+        }
+        return null;
     }
 
     @Override
     public void forEach(BiConsumer<K, V> consumer) {
-        throw new UnsupportedOperationException();
+        if (array == null) {
+            return;
+        }
+        for (int i = 0; i < capacity; i++) {
+            var node = array[i];
+            while (node != null) {
+                consumer.accept(node.key, node.value);
+                node = node.next;
+            }
+        }
     }
 
     @SuppressWarnings("unchecked")
@@ -74,6 +133,7 @@ public final class SeparateChainingMap<K, V> implements Map<K, V> {
     private static final class Node<K, V> {
         K key;
         V value;
+
         Node<K, V> prev;
         Node<K, V> next;
 
@@ -81,5 +141,58 @@ public final class SeparateChainingMap<K, V> implements Map<K, V> {
             this.key = key;
             this.value = value;
         }
+
+        public void add(Node<K, V> node) {
+            var curNode = this;
+            while (curNode.next != null) {
+                curNode = curNode.next;
+            }
+            curNode.next = node;
+            curNode.next.prev = curNode;
+        }
+
+        public boolean equals(Node<K, V> o) {
+            if (o == this) {
+                return true;
+            }
+            return Objects.equals(key, o.key) && Objects.equals(value, o.value);
+        }
+
     }
+
+    private int hash(K key) {
+        int h;
+        return (key == null) ? 0 : (h = key.hashCode()) ^ (h >>> 16);
+    }
+
+    private void resize() {
+        expectedMaxSize *= 2;
+        capacity *= 2;
+        var oldArray = array;
+        array = allocate(capacity);
+        for (int i = 0; i < capacity / 2; i++) {
+            var node = oldArray[i];
+            while (node != null) {
+                put(node.key, node.value);
+                node = node.next;
+            }
+        }
+    }
+
+    private int indexOf(int h, int capacity) {
+        return h & (capacity - 1);
+    }
+
+    private Node<K, V> getNode(K key) {
+        int index = indexOf(hash(key), capacity);
+        Node<K, V> node = array[index];
+        while (node != null) {
+            if (node.key.equals(key)) {
+                return node;
+            }
+            node = node.next;
+        }
+        return null;
+    }
+
 }
