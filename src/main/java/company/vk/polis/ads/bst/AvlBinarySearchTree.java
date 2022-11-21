fcda12d318288471866c1d19b7f32d2006c54d4b
osokindm <osokin34@gmail.com>
2022-11-21 23:04:55 +0300

    8th hw balance by deletion fix

--- src/main/java/company/vk/polis/ads/bst/AvlBinarySearchTree.java
+++ src/main/java/company/vk/polis/ads/bst/AvlBinarySearchTree.java
@@ -50,59 +50,6 @@ public class AvlBinarySearchTree<Key extends Comparable<Key>, Value> implements 
         root = put(root, key, value);
     }
 
-    private Node put(Node x, Key key, Value value) {
-        if (x == null) {
-            return new Node(key, value, 1);
-        }
-        if (key.compareTo(x.key) < 0) {
-            x.left = put(x.left, key, value);
-        } else if (key.compareTo(x.key) > 0) {
-            x.right = put(x.right, key, value);
-        } else {
-            x.value = value;
-            size--; // when updating a value size remains the same
-        }
-        fixHeight(x);
-        x = balance(x);
-        return x;
-    }
-
-    private Node rotateRight(Node y) {
-        Node x = y.left;
-        y.left = x.right;
-        x.right = y;
-        fixHeight(y);
-        fixHeight(x);
-        return x;
-    }
-
-    private Node rotateLeft(Node x) {
-        Node y = x.right;
-        x.right = y.left;
-        y.left = x;
-        fixHeight(x);
-        fixHeight(y);
-        return y;
-    }
-
-    private int factor(Node x) {
-        return x == null ? 0 : height(x.left) - height(x.right);
-    }
-
-    private Node balance(Node x) {
-        if (factor(x) == 2) {
-            if (factor(x.left) < 0)
-                x.left = rotateLeft(x.left);
-            return rotateRight(x);
-        }
-        if (factor(x) == -2) {
-            if (factor(x.right) > 0)
-                x.right = rotateRight(x.right);
-            return rotateLeft(x);
-        }
-        return x;
-    }
-
     @Override
     public Value remove(@NotNull Key key) {
         Value deleted = get(key);
@@ -112,56 +59,6 @@ public class AvlBinarySearchTree<Key extends Comparable<Key>, Value> implements 
         root = balance(delete(root, key));
         size--;
         return deleted;
-    }
-
-    private Node delete(Node x, Key key) {
-        if (x == null) return null;
-        if (key.compareTo(x.key) < 0) {
-            x.left = delete(x.left, key);
-        }
-        if (key.compareTo(x.key) > 0) {
-            x.right = delete(x.right, key);
-        }
-        if (key == x.key) {
-            x = innerDelete(x);
-        }
-        return x;
-    }
-
-    private Node innerDelete(Node x) {
-        if (x.right == null) {
-            return x.left;
-        }
-        if (x.left == null) {
-            return x.right;
-        }
-        Node t = x;
-        x = min(t.right);
-        x.right = deleteMin(t.right);
-        x.left = t.left;
-        return x;
-    }
-
-    private Node deleteMin(Node x) {
-        if (x.left == null) {
-            return x.right;
-        }
-        x.left = deleteMin(x.left);
-        return x;
-    }
-
-    private Node min(Node x) {
-        while (x.left != null) {
-            x = x.left;
-        }
-        return x;
-    }
-
-    private Node max(Node x) {
-        while (x.right != null) {
-            x = x.right;
-        }
-        return x;
     }
 
     @Override
@@ -233,6 +130,120 @@ public class AvlBinarySearchTree<Key extends Comparable<Key>, Value> implements 
     }
 
     private void fixHeight(Node x) {
+        if (x == null) {
+            return;
+        }
         x.height = 1 + Math.max(height(x.left), height(x.right));
+    }
+
+    private Node put(Node x, Key key, Value value) {
+        if (x == null) {
+            return new Node(key, value, 1);
+        }
+        if (key.compareTo(x.key) < 0) {
+            x.left = put(x.left, key, value);
+        } else if (key.compareTo(x.key) > 0) {
+            x.right = put(x.right, key, value);
+        } else {
+            x.value = value;
+            size--; // when updating a value size remains the same
+        }
+        fixHeight(x);
+        x = balance(x);
+        return x;
+    }
+
+    private Node rotateRight(Node y) {
+        Node x = y.left;
+        y.left = x.right;
+        x.right = y;
+        fixHeight(y);
+        fixHeight(x);
+        return x;
+    }
+
+    private Node rotateLeft(Node x) {
+        Node y = x.right;
+        x.right = y.left;
+        y.left = x;
+        fixHeight(x);
+        fixHeight(y);
+        return y;
+    }
+
+    private int factor(Node x) {
+        return x == null ? 0 : height(x.left) - height(x.right);
+    }
+
+    private Node balance(Node x) {
+        if (factor(x) == 2) {
+            if (factor(x.left) < 0)
+                x.left = rotateLeft(x.left);
+            return rotateRight(x);
+        }
+        if (factor(x) == -2) {
+            if (factor(x.right) > 0)
+                x.right = rotateRight(x.right);
+            return rotateLeft(x);
+        }
+        return x;
+    }
+
+    private Node delete(Node x, Key key) {
+        if (x == null) {
+            return null;
+        }
+        if (key.compareTo(x.key) < 0) {
+            x.left = delete(x.left, key);
+        }
+        if (key.compareTo(x.key) > 0) {
+            x.right = delete(x.right, key);
+        }
+        if (key.compareTo(x.key) == 0) {
+            x = innerDelete(x);
+        }
+        fixHeight(x);
+        x = balance(x);
+        return x;
+    }
+
+    private Node innerDelete(Node x) {
+        if (x.right == null) {
+            return x.left;
+        }
+        if (x.left == null) {
+            return x.right;
+        }
+        Node t = x;
+        x = min(t.right);
+        x.right = deleteMin(t.right);
+        x.left = t.left;
+        fixHeight(x);
+        x = balance(x);
+        return x;
+    }
+
+    private Node deleteMin(Node x) {
+        if (x.left == null) {
+            return x.right;
+        }
+        x.left = deleteMin(x.left);
+        fixHeight(x);
+        x = balance(x);
+        return x;
+    }
+
+    private Node min(Node x) {
+        while (x.left != null) {
+            x = x.left;
+        }
+        return x;
+    }
+
+    private Node max(Node x) {
+        while (x.right != null) {
+            x = x.right;
+        }
+        return x;
     }
 }
