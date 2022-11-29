f7e7b4e128ccfbf5a31304d41c50cdf84b2d8cf5
Denis Strizhkin <strdenis02@gmail.com>
2022-11-29 19:11:01 +0300

    Finished scc.

--- src/main/java/company/vk/polis/ads/part4/denisstrizhkin/scc/Main.java
+++ src/main/java/company/vk/polis/ads/part4/denisstrizhkin/scc/Main.java
@@ -6,45 +6,44 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
-    // https://www.eolymp.com/ru/submissions/12306318 (56%)
     private static int[] Vid;
     private static int[] Vflags;
     private static int[] VRflags;
     private static List<List<Integer>> V;
     private static List<List<Integer>> Vr;
 
-    private static final List<Integer> order = new ArrayList<>();
-    private static final List<List<Integer>> components = new ArrayList<>();
+    private static final List<Integer> topSorted = new ArrayList<>();
+    private static int curId;
 
     private Main() {
         // Should not be instantiated
     }
 
-    private static void dfs1(int v) {
-        Vflags[v] = 1;
-        for (int vertex : V.get(v)) {
-            if (Vflags[vertex] == 0) {
-                dfs1(vertex);
+    static void dfs1(int curV) {
+        Vflags[curV] = 1;
+        for (int nextV : V.get(curV)) {
+            if (Vflags[nextV] == 0) {
+                dfs1(nextV);
             }
         }
-        Vflags[v] = -1;
-        order.add(v);
+        topSorted.add(curV);
     }
 
-    private static void dfs2(int v) {
-        VRflags[v] = 1;
-        components.get(components.size() - 1).add(v);
-        Vid[v] = components.size() - 1;
-        for (int vertex : Vr.get(v)) {
-            if (VRflags[vertex] == 0) {
-                dfs2(vertex);
+    static void dfs2(int curV) {
+        VRflags[curV] = 1;
+        Vid[curV] = curId;
+        for (int nextV : Vr.get(curV)) {
+            if (VRflags[nextV] == 0) {
+                dfs2(nextV);
             }
         }
-        VRflags[v] = -1;
     }
 
     private static void solve(final FastScanner in, final PrintWriter out) {
@@ -68,31 +67,29 @@ public class Main {
             Vr.get(y).add(x);
         }
 
-        for (int i = 1; i <= n; i++) {
-            if (Vflags[i] == 0) {
-                dfs1(i);
+        for (int v = 1; v <= n; v++) {
+            if (Vflags[v] == 0) {
+                dfs1(v);
             }
         }
 
-        for (int i = 0; i < n; i++) {
-            int v = order.get(n - 1 - i);
+        for (int i = topSorted.size() - 1; i >= 0; i--) {
+            int v = topSorted.get(i);
             if (VRflags[v] == 0) {
-                components.add(new ArrayList<>());
+                curId++;
                 dfs2(v);
             }
         }
 
-        //out.println(components);
-        //out.println(Arrays.toString(Vid));
-        int count = 0;
+        Set<List<Integer>> condE = new HashSet<>();
         for (int start = 1; start < V.size(); start++) {
             for (int end : V.get(start)) {
                 if (Vid[start] != Vid[end]) {
-                    count++;
+                    condE.add(new ArrayList<>(Arrays.asList(Vid[start], Vid[end])));
                 }
             }
         }
-        out.println(count);
+        out.println(condE.size());
     }
 
     private static final class FastScanner {
