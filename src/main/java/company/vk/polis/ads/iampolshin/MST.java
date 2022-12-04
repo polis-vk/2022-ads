ad0d881d0d56b33bb434df245a7634e34b2049d2
iampolshin <i.am.polshin@gmail.com>
2022-12-05 00:10:32 +0300

    MST: fixed indexing

--- src/main/java/company/vk/polis/ads/iampolshin/MST.java
+++ src/main/java/company/vk/polis/ads/iampolshin/MST.java
@@ -29,7 +29,7 @@ public final class MST {
         Arrays.sort(graph);
 
         int[] treeId = new int[n + 1];
-        for (int i = 0; i <= n; i++) {
+        for (int i = 1; i <= n; i++) {
             treeId[i] = i;
         }
 
