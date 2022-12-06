2a97a60cef7bb19407b1a94218c3ef463dc85a39
charlsraven <voron00100867@gmail.com>
2022-12-06 13:01:43 +0300

    add DangerousPath

---
+++ src/main/java/DangerousPath.java
@@ -0,0 +1,91 @@
+//https://www.eolymp.com/ru/submissions/12384244
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class DangerousPath {
+    public static void main(String[] args) {
+        try (Scanner in = new Scanner(System.in)) {
+            int n = in.nextInt();
+            int m = in.nextInt();
+            Graph graph = new Graph(n, m);
+            for (int i = 0; i < m; i++) {
+                graph.edge[i] = new Graph.Edge(in.nextInt(), in.nextInt(), in.nextInt());
+            }
+            System.out.println(graph.kruskal());
+        }
+    }
+
+    private static class Graph {
+        record Edge(int src, int dest, int weight) implements Comparable<Edge> {
+
+            @Override
+            public int compareTo(Edge that) {
+                return this.weight - that.weight;
+            }
+        }
+
+        private static class Subset {
+            int parent;
+            int rank;
+        }
+
+        private final int vertices;
+        private final Edge[] edge;
+
+        Graph(int v, int e) {
+            vertices = v;
+            edge = new Edge[e];
+        }
+
+        int kruskal() {
+            Arrays.sort(edge);
+            Subset[] subsets = new Subset[vertices + 1];
+            for (int i = 0; i < vertices + 1; i++) {
+                subsets[i] = new Subset();
+            }
+
+            for (int i = 0; i < vertices; i++) {
+                subsets[i].parent = i;
+                subsets[i].rank = 0;
+            }
+            int index = 0;
+            int e = 0;
+            Edge nextEdge = new Edge(0,0,0);
+            while (e < vertices - 1) {
+                 nextEdge = edge[index++];
+                int x = find(subsets, nextEdge.src);
+                int y = find(subsets, nextEdge.dest);
+                if (x != y) {
+                    e++;
+                    union(subsets, x, y);
+                }
+                x = find(subsets, 1);
+                y = find(subsets, subsets.length - 1);
+                if (x == y) {
+                    break;
+                }
+            }
+            return nextEdge.weight;
+        }
+
+        private int find(Subset[] subsets, int i) {
+            if (subsets[i].parent != i) {
+                subsets[i].parent = find(subsets, subsets[i].parent);
+            }
+            return subsets[i].parent;
+        }
+
+        private void union(Subset[] subsets, int x, int y) {
+            int xroot = find(subsets, x);
+            int yroot = find(subsets, y);
+            if (subsets[xroot].rank < subsets[yroot].rank) {
+                subsets[xroot].parent = yroot;
+            } else if (subsets[xroot].rank > subsets[yroot].rank) {
+                subsets[yroot].parent = xroot;
+            } else {
+                subsets[yroot].parent = xroot;
+                subsets[xroot].rank++;
+            }
+        }
+    }
+}
