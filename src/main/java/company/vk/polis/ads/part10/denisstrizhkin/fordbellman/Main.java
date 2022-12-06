ec34d0af5a5e4b16336bc1e4fdc929f795d70635
Denis Strizhkin <strdenis02@gmail.com>
2022-12-06 17:45:59 +0300

    fordbellman 100%.

--- src/main/java/company/vk/polis/ads/part10/denisstrizhkin/fordbellman/Main.java
+++ src/main/java/company/vk/polis/ads/part10/denisstrizhkin/fordbellman/Main.java
@@ -5,34 +5,55 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    private static List<List<Integer>> V;
+    private final static int MAX_DIST = 30_000;
+    private static int[][] edges;
 
     private Main() {
         // Should not be instantiated
     }
 
+    private static int[] solve(int n) {
+        int[] dist = new int[n + 1];
+        Arrays.fill(dist, MAX_DIST);
+        dist[1] = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            boolean relaxFlag = false;
+            for (int[] edge : edges) {
+                if (dist[edge[0]] < MAX_DIST) {
+                    if (dist[edge[1]] > dist[edge[0]] + edge[2]) {
+                        dist[edge[1]] = dist[edge[0]] + edge[2];
+                        relaxFlag = true;
+                    }
+                }
+            }
+            if (!relaxFlag) {
+                break;
+            }
+        }
+
+        return dist;
+    }
+
     private static void solve(final FastScanner in, final PrintWriter out) {
         int n = in.nextInt();
         int m = in.nextInt();
-
-        V = new ArrayList<>();
-        for (int i = 0; i <= n; i++) {
-            V.add(new ArrayList<>());
-        }
+        edges = new int[m][3];
 
         for (int i = 0; i < m; i++) {
-            int x = in.nextInt();
-            int y = in.nextInt();
-            V.get(x).add(y);
-            V.get(y).add(x);
+            edges[i][0] = in.nextInt();
+            edges[i][1] = in.nextInt();
+            edges[i][2] = in.nextInt();
         }
 
-        out.println(V.size());
+        int[] dist = solve(n);
+        for (int i = 1; i <= n; i++) {
+            out.print(dist[i] + " ");
+        }
     }
 
     private static final class FastScanner {
