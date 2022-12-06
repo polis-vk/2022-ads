71465a2f0510c4e5992455bac275458a4cb36b03
llav3ji2019 <pavelemelyanov03@mail.ru>
2022-12-06 07:04:25 +0300

    fixing

--- src/main/java/company/vk/polis/ads/MinTree.java
+++ src/main/java/company/vk/polis/ads/MinTree.java
@@ -42,7 +42,7 @@ public final class MinTree {
         out.println(result);
     }
 
-    private static final class Edge implements Comparable<Edge> {
+    private static class Edge implements Comparable<Edge> {
         public int left;
         public int right;
         public int weight;
