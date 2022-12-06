54e3c6607b408227266ae477e46d1ede07330925
Denis Strizhkin <strdenis02@gmail.com>
2022-12-06 13:05:03 +0300

    Added solve templates.

--- src/main/java/company/vk/polis/ads/part10/denisstrizhkin/fordbellman/Main.java
+++ src/main/java/company/vk/polis/ads/part10/denisstrizhkin/fordbellman/Main.java
@@ -1,4 +1,68 @@
 package company.vk.polis.ads.part10.denisstrizhkin.fordbellman;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
 public class Main {
+    private static List<List<Integer>> V;
+
+    private Main() {
+        // Should not be instantiated
+    }
+
+    private static void solve(final FastScanner in, final PrintWriter out) {
+        int n = in.nextInt();
+        int m = in.nextInt();
+
+        V = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            V.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < m; i++) {
+            int x = in.nextInt();
+            int y = in.nextInt();
+            V.get(x).add(y);
+            V.get(y).add(x);
+        }
+
+        out.println(V.size());
+    }
+
+    private static final class FastScanner {
+        private final BufferedReader reader;
+        private StringTokenizer tokenizer;
+
+        FastScanner(final InputStream in) {
+            reader = new BufferedReader(new InputStreamReader(in));
+        }
+
+        String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+
+    public static void main(final String[] arg) {
+        final FastScanner in = new FastScanner(System.in);
+        try (PrintWriter out = new PrintWriter(System.out)) {
+            solve(in, out);
+        }
+    }
 }
