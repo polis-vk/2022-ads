88db654cabbe975e17d61346344d4a25e82d245f
llav3ji2019 <pavelemelyanov03@mail.ru>
2022-12-06 05:39:51 +0300

    fixing

---
+++ src/main/java/company/vk/polis/ads/FordBellman.java
@@ -0,0 +1,50 @@
+package company.vk.polis.ads;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class FordBellman {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int m = in.nextInt();
+
+        List<Edge> edges = new ArrayList<>(m);
+        for (int i = 0; i < m; ++i) {
+            int left = in.nextInt();
+            int right = in.nextInt();
+            int weight = in.nextInt();
+            edges.add(new Edge(left, right, weight));
+        }
+
+        int [] distance = new int[n + 1];
+        Arrays.fill(distance, Integer.MAX_VALUE);
+        distance[1] = 0;
+
+        for (int i = 0; i < n; i++) {
+            for(Edge e : edges) {
+                if (distance[e.left] != Integer.MAX_VALUE) {
+                    distance[e.right] = Math.min(distance[e.left] + e.weight, distance[e.right]);
+                }
+            }
+        }
+        for (int i = 1; i <= n; i++) {
+            int result = distance[i] == Integer.MAX_VALUE ? 30000 : distance[i];
+            System.out.print(result + " ");
+        }
+    }
+
+    private static class Edge  {
+        public int left;
+        public int right;
+        public int weight;
+
+        public Edge(int left, int right, int weight) {
+            this.left = left;
+            this.right = right;
+            this.weight = weight;
+        }
+    }
+}
--- src/main/java/company/vk/polis/ads/ShortestWay.java
+++ src/main/java/company/vk/polis/ads/ShortestWay.java
@@ -2,16 +2,25 @@ package company.vk.polis.ads;
 
 import java.util.Arrays;
 import java.util.LinkedList;
-import java.util.Queue;
+import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class ShortestWay {
-    private static class Edge {
-        int vertex;
-        int weight;
+    private static class Edge implements Comparable<Edge>  {
+        public int vertex;
+        public int weight;
+
         public Edge(int vertex, int weight) {
             this.vertex = vertex;
             this.weight = weight;
+        }
+
+        @Override
+        public int compareTo(Edge other) {
+            if (weight != other.weight) {
+                return weight - other.weight;
+            }
+            return vertex - other.vertex;
         }
     }
 
@@ -21,23 +30,23 @@ public class ShortestWay {
         int m = in.nextInt();
         int s = in.nextInt();
         int f = in.nextInt();
-        int[][] weights = new int[n + 1][n + 1];
-        for (int[] weight : weights) {
+        int[][] graph = new int[n + 1][n + 1];
+        for (int[] weight : graph) {
             Arrays.fill(weight, Integer.MAX_VALUE);
         }
         for (int i = 0; i < m; i++) {
             int left = in.nextInt();
             int right = in.nextInt();
             int weight = in.nextInt();
-            weights[left][right] = weight;
-            weights[right][left] = weight;
+            graph[left][right] = weight;
+            graph[right][left] = weight;
         }
         int[] parent = new int[n + 1];
         int[] distance = new int[n + 1];
         Arrays.fill(distance, Integer.MAX_VALUE);
         distance[s] = 0;
         Arrays.fill(parent, -1);
-        Queue<Edge> container = new LinkedList<>();
+        PriorityQueue<Edge> container = new PriorityQueue<>();
         container.add(new Edge(s, 0));
         while (!container.isEmpty()) {
             Edge currentEdge = container.poll();
@@ -45,8 +54,8 @@ public class ShortestWay {
                 continue;
             }
             for (int i = 1; i < n + 1; i++) {
-                if (weights[currentEdge.vertex][i] != Integer.MAX_VALUE) {
-                    int newDist = distance[currentEdge.vertex] + weights[currentEdge.vertex][i];
+                if (graph[currentEdge.vertex][i] != Integer.MAX_VALUE) {
+                    int newDist = distance[currentEdge.vertex] + graph[currentEdge.vertex][i];
                     if (distance[i] > newDist) {
                         distance[i] = newDist;
                         parent[i] = currentEdge.vertex;
