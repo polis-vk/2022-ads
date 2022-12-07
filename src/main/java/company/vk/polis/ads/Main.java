c95b36650ffbdef172530141aa1c104a276b8e0c
AlexAndrovSPBSTU <AleksandrovAlexey0929@yandex.ru>
2022-12-07 14:07:56 +0300

    initial commit

---
+++ src/main/java/company/vk/polis/ads/Main.java
@@ -0,0 +1,112 @@
+package company.vk.polis.ads;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+/**
+ * Problem solution template.
+ *
+ * @author Dmitry Schitinin
+ */
+
+
+public final class Main {
+    private static List<List<Integer>> listOfList;
+    private static List<List<Integer>> listOfList1;
+    private static List<Integer> list = new LinkedList<>();
+    private static byte[] isVisited;
+
+
+    private Main() {
+        // Should not be instantiated
+    }
+
+    private static void solve(final FastScanner in, final PrintWriter out) {
+        int vertexNumber = in.nextInt();
+        int edgesNumber = in.nextInt();
+        listOfList = new ArrayList<>();
+        listOfList1 = new ArrayList<>();
+        isVisited = new byte[vertexNumber + 1];
+
+        for (int i = 0; i <= vertexNumber; i++) {
+            listOfList.add(new ArrayList<>());
+            listOfList1.add(new ArrayList<>());
+        }
+        for (int i = 0; i < edgesNumber; i++) {
+            int a = in.nextInt();
+            int b = in.nextInt();
+            listOfList.get(a).add(b);
+            listOfList1.get(b).add(a);
+        }
+        topsort(vertexNumber);
+        if (!flag && !list.isEmpty())
+            list.forEach(e -> System.out.print(e + " "));
+        else
+            System.out.println(-1);
+    }
+
+    private static void topsort(int edgesNumber) {
+        for (int i = 1; i <= edgesNumber; i++) {
+            if (isVisited[i] == 0) {
+                dfs(i);
+            }
+        }
+    }
+
+    private static boolean flag = false;
+
+    private static void dfs(int node) {
+        isVisited[node] = 1;
+        for (int neighboor : listOfList.get(node)) {
+            //Узел не посетили
+            if (isVisited[neighboor] == 0) {
+                dfs(neighboor);
+            }
+            //Узел серый
+            if (isVisited[neighboor] == 1) {
+                flag = true;
+                return;
+            }
+        }
+        isVisited[node] = 2;
+        list.add(0, node);
+    }
+
+    private static final class FastScanner {
+        private final BufferedReader reader;
+        private StringTokenizer tokenizer;
+
+        FastScanner(final InputStream in) {
+            reader = new BufferedReader(new InputStreamReader(in));
+        }
+
+        String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+
+    public static void main(final String[] arg) {
+        final FastScanner in = new FastScanner(System.in);
+        try (PrintWriter out = new PrintWriter(System.out)) {
+            solve(in, out);
+        }
+    }
+}
