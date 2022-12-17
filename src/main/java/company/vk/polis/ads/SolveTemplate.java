fda14c355dd7f6668d671f033137984fadb81a96
artemokky <ferrumsir@gmail.com>
2022-12-17 16:36:19 +0300

    some fix

--- src/main/java/company/vk/polis/ads/SolveTemplate.java
+++ src/main/java/company/vk/polis/ads/SolveTemplate.java
@@ -21,7 +21,7 @@ public final class SolveTemplate {
         // Write me
     }
 
-    static final class FastScanner {
+    private gstatic final class FastScanner {
         private final BufferedReader reader;
         private StringTokenizer tokenizer;
 
